ae02617782d10315c9b6bfb75400b0a7f059b368
manuelcruz98 <1231841@isep.ipp.pt>
2024-02-28 12:02:29 +0000

    fixing errors that were made on SensorDTO class and Sensor Interface

--- src/main/java/SmartHome/dto/SensorDTO.java
+++ src/main/java/SmartHome/dto/SensorDTO.java
@@ -7,11 +7,9 @@ import java.util.List;
 
 public class SensorDTO {
     private final SensorType _sensorType;
-    private List<String> _listStringClassesSensors;
 
     public SensorDTO(Sensor sensor) {
 
         this._sensorType= sensor.getSensorType();
-        this._listStringClassesSensors = sensor.getSensorList();
     }
 }
