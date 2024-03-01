c47fd3946629aafd72fc9eb7da64054bded985fa
Lloyd <1231838@isep.ipp.pt>
2024-03-01 17:24:11 +0000

    #2 US14 (work in progress) - BlindRoolerValue Class + Test

---
+++ src/main/java/SmartHome/actuators/BlindRollerValue.java
@@ -0,0 +1,37 @@
+package SmartHome.actuators;
+
+import SmartHome.domain.Value;
+
+public class BlindRollerValue implements Value, Cloneable {
+
+    private int _nValue;
+
+    /**
+     * Constructor of the class.
+     *
+     * @param nValue The value of the blind roller.
+     */
+    public BlindRollerValue(int nValue) {
+        this._nValue = nValue;
+    }
+
+    /**
+     * Clones the value of the blind roller.
+     *
+     * @return The cloned value of the blind roller.
+     */
+    @Override
+    public BlindRollerValue clone() {
+        try {
+            // Call the Object clone() method
+            return (BlindRollerValue) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This should never happen since we are Cloneable
+            throw new AssertionError();
+        }
+    }
+    public int get_nValue() {
+        return _nValue;
+    }
+
+}
