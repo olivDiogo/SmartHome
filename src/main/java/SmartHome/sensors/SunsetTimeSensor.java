cac6893851edc83e281a5e782cdc35b6f768ee0e
rueloParente <1231858@isep.ipp.pt>
2024-03-01 16:56:42 +0000

    #9 Implemented sensor interface methods

--- src/main/java/SmartHome/sensors/SunsetTimeSensor.java
+++ src/main/java/SmartHome/sensors/SunsetTimeSensor.java
@@ -1,21 +1,25 @@
 package SmartHome.sensors;
 
 import SmartHome.domain.*;
+import org.shredzone.commons.suncalc.SunTimes;
 
-public class SunsetTimeSensor {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class SunsetTimeSensor implements Sensor {
     private SensorType _sensorType;
     private Gps _gps;
     public SunsetTimeSensor(CatalogueSensor catalogueSensor) throws InstantiationException {
     setSensorType(catalogueSensor);
     }
 
-    private SensorType setSensorType(CatalogueSensor catalogue) throws InstantiationException {
+    private void setSensorType(CatalogueSensor catalogue) throws InstantiationException {
         SensorType sensorType = catalogue.getSensorType("SunsetTime");
         if (sensorType == null)
             throw new InstantiationException("SensorType with description 'SunsetTime' does not exist.");
         else {
             this._sensorType = sensorType;
-            return sensorType;
         }
     }
     public Gps configureGpsLocation(Gps gps) {
@@ -26,6 +30,18 @@ public class SunsetTimeSensor {
             return gps;
         }
     }
+
+    public SensorType getSensorType() {
+        return this._sensorType;
+    }
+
+    //Default behavior will return sunset for current day
+    public Value getValue() {
+        SunTimes time = SunTimes.compute().on(LocalDate.now()).at(this._gps.getLatitude(), this._gps.getLongitude()).execute();
+        LocalTime sunset = Objects.requireNonNull(time.getSet()).toLocalTime();
+        return new SunsetTimeValue(sunset);
+    }
+
 
 
 }
