78f5850c4b7e152e1de538f404ba2ee356ded07b
Lloyd <1231838@isep.ipp.pt>
2024-03-02 18:04:44 +0000

    #34 Implemetation of Mockito (after version 3.4) in AtuatorTypeFactory

--- src/test/java/SmartHome/domain/ActuatorTypeFactoryTest.java
+++ src/test/java/SmartHome/domain/ActuatorTypeFactoryTest.java
@@ -1,6 +1,12 @@
 package SmartHome.domain;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class ActuatorTypeFactoryTest {
 
@@ -9,12 +15,26 @@ public class ActuatorTypeFactoryTest {
      *
      * @throws InstantiationException if the description is null
      */
+
     @Test
     void shouldReturnObjectWhenValidDescription() throws InstantiationException {
         //Arrange
         String description = "description";
 
-        //Act
-        new ActuatorTypeFactory().createActuatorType(description);
+            try (MockedConstruction<ActuatorType> mocked = Mockito.mockConstruction(ActuatorType.class, (mock, context) -> {
+                when(mock.getDescription()).thenReturn(description);
+            })){;
+            {
+                //Act
+                ActuatorTypeFactory actuatorTypeFactory = new ActuatorTypeFactory();
+                ActuatorType actuatorType = actuatorTypeFactory.createActuatorType(description);
+                //Assert
+                assertEquals(description, actuatorType.getDescription());
+
+            }
+        }
     }
+
+
 }
+
