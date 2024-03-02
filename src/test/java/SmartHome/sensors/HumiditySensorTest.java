045d9027ac66b72d763c0bc48fb5348a0b74bbca
BernardoAmorim <1110281@isep.ipp.pt>
2024-03-02 16:57:26 +0000

    Update HumiditySensor tests to use MockedConstruction

--- src/test/java/SmartHome/sensors/HumiditySensorTest.java
+++ src/test/java/SmartHome/sensors/HumiditySensorTest.java
@@ -4,10 +4,10 @@ import SmartHome.domain.*;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.plist.PropertyListConfiguration;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class HumiditySensorTest {
     @Test
@@ -57,21 +57,74 @@ class HumiditySensorTest {
     }
 
     @Test
-    void getValueReturnsCorrectValue() throws InstantiationException {
+    void getValueReturnsPositiveValue() throws InstantiationException {
         // Arrange
         String description = "Humidity";
+        String humidity = "100";
         CatalogueSensor catalogueDouble = mock(CatalogueSensor.class);
         SensorType sensorTypeDouble = mock(SensorType.class);
 
         when(catalogueDouble.getSensorType(description)).thenReturn(sensorTypeDouble);
 
-        HumiditySensor humiditySensor = new HumiditySensor( catalogueDouble);
+        try(MockedConstruction<HumiditySensorValue> humiditySensorValue = mockConstruction(HumiditySensorValue.class, (mock, context) -> {
+            when(mock.clone()).thenReturn(mock);
+            when(mock.toString()).thenReturn(humidity);
+        })) {
+            HumiditySensor humiditySensor = new HumiditySensor( catalogueDouble);
 
-        // Act
-        Value value = humiditySensor.getValue();
-        double humidityValue = Double.parseDouble(value.toString());
+            // Act
+            double result =Double.parseDouble(humiditySensor.getValue().toString());
 
-        // Assert
-        assertTrue(humidityValue >= 0 && humidityValue <= 100);
+            // Assert
+            assertTrue(result >= 0 && result <= 100, "The humidity value should be between 0 and 100.");
+        }
+    }
+
+    @Test
+    void getValueReturnsZero() throws InstantiationException {
+        // Arrange
+        String description = "Humidity";
+        String humidity = "0";
+        CatalogueSensor catalogueDouble = mock(CatalogueSensor.class);
+        SensorType sensorTypeDouble = mock(SensorType.class);
+
+        when(catalogueDouble.getSensorType(description)).thenReturn(sensorTypeDouble);
+
+        try(MockedConstruction<HumiditySensorValue> humiditySensorValue = mockConstruction(HumiditySensorValue.class, (mock, context) -> {
+            when(mock.clone()).thenReturn(mock);
+            when(mock.toString()).thenReturn(humidity);
+        })) {
+            HumiditySensor humiditySensor = new HumiditySensor( catalogueDouble);
+
+            // Act
+            double result =Double.parseDouble(humiditySensor.getValue().toString());
+
+            // Assert
+            assertTrue(result >= 0 && result <= 100,"The humidity value should be between 0 and 100.");
+        }
+    }
+
+    @Test
+    void getValueReturnsInvalidValue() throws InstantiationException {
+        // Arrange
+        String description = "Humidity";
+        String humidity = "-1";
+        CatalogueSensor catalogueDouble = mock(CatalogueSensor.class);
+        SensorType sensorTypeDouble = mock(SensorType.class);
+
+        when(catalogueDouble.getSensorType(description)).thenReturn(sensorTypeDouble);
+
+        try(MockedConstruction<HumiditySensorValue> humiditySensorValue = mockConstruction(HumiditySensorValue.class, (mock, context) -> {
+            when(mock.clone()).thenReturn(mock);
+            when(mock.toString()).thenReturn(humidity);
+        })) {
+            HumiditySensor humiditySensor = new HumiditySensor( catalogueDouble);
+
+            // Act
+            double result =Double.parseDouble(humiditySensor.getValue().toString());
+
+            // Assert
+            assertFalse(result >= 0 && result <= 100,"The humidity value should be between 0 and 100.");
+        }
     }
 }
