820e745c43eb38114c9eed58c63448e179dceaac
simao-campos87 <1231859@isep.ipp.pt>
2024-03-04 10:12:51 +0000

    Issue #8 classes and tests for SunriseSensor using a given date

--- src/test/java/SmartHome/sensors/SunriseSensorTest.java
+++ src/test/java/SmartHome/sensors/SunriseSensorTest.java
@@ -7,6 +7,8 @@ import SmartHome.domain.Value;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -121,15 +123,54 @@ public class SunriseSensorTest {
             when(sunriseTimeValueDouble.toString()).thenReturn(value);
         })) {
 
-        //Act
-        Value result = sunriseTimeSensor.getValue();
-        System.out.println(result.toString());
-        //Assert
-        List<SunriseTimeValue> sunriseTimeSensorValues = mocked.constructed();
-       // assertTrue(sunriseTimeSensorValues.contains(result));
-        assertEquals(result.toString(), sunriseTimeSensorValues.get(0).toString());
-        assertEquals(expectedSize, sunriseTimeSensorValues.size());
+            //Act
+            Value result = sunriseTimeSensor.getValue();
+            System.out.println(result.toString());
+            //Assert
+            List<SunriseTimeValue> sunriseTimeSensorValues = mocked.constructed();
+            // assertTrue(sunriseTimeSensorValues.contains(result));
+            assertEquals(result.toString(), sunriseTimeSensorValues.get(0).toString());
+            assertEquals(expectedSize, sunriseTimeSensorValues.size());
+        }
+
     }
 
-}
+    @Test
+    void shouldReturnSunriseTimeValueForSpecificDay() throws InstantiationException {
+        //Arrange
+        String description = "SunriseTime";
+        int expectedSize = 1;
+        String value = "Sunrise Time: 06:00:00";
+        LocalDate date = LocalDate.of(2021, 10, 10);
+
+        CatalogueSensor catalogueSensorDouble = mock(CatalogueSensor.class);
+        SensorType sensorTypeDouble = mock(SensorType.class);
+        SunriseTimeValue sunriseTimeValueDouble = mock(SunriseTimeValue.class);
+
+        when(catalogueSensorDouble.getSensorType(description)).thenReturn(sensorTypeDouble);
+
+        Gps gpsDouble = mock(Gps.class);
+        when(gpsDouble.getLatitude()).thenReturn(41.1579); // Coordinates to oporto
+        when(gpsDouble.getLongitude()).thenReturn(8.6291);
+
+        SunriseTimeSensor sunriseTimeSensor = new SunriseTimeSensor(catalogueSensorDouble);
+        sunriseTimeSensor.configureGpsLocation(gpsDouble);
+
+        try (MockedConstruction<SunriseTimeValue> mocked = mockConstruction(SunriseTimeValue.class, (mock, context) -> {
+            when(mock.clone()).thenReturn(sunriseTimeValueDouble);
+            when(mock.toString()).thenReturn(value);
+            when(sunriseTimeValueDouble.toString()).thenReturn(value);
+        })) {
+
+            //Act
+            Value result = sunriseTimeSensor.getValue(date);
+            System.out.println(result.toString());
+            //Assert
+            List<SunriseTimeValue> sunriseTimeSensorValues = mocked.constructed();
+            // assertTrue(sunriseTimeSensorValues.contains(result));
+            assertEquals(result.toString(), sunriseTimeSensorValues.get(0).toString());
+            assertEquals(expectedSize, sunriseTimeSensorValues.size());
+        }
+
+    }
 }
