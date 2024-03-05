f16e1ab850e234aafe835c47bee7380d1656d202
simao-campos87 <1231859@isep.ipp.pt>
2024-03-05 10:59:05 +0000

    issues #20 class name refactor

--- src/test/java/SmartHome/sensors/AveragePowerConsumptionSensorValueTest.java
+++ src/test/java/SmartHome/sensors/AveragePowerConsumptionSensorValueTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class PowerConsumptionSensorValueTest {
+public class AveragePowerConsumptionSensorValueTest {
 
     /**
      * See if the constructor works.
