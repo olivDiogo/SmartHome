241340bdafb8ecd855e96105aaf08e38d63de304
rueloParente <1231858@isep.ipp.pt>
2024-03-05 23:57:44 +0000

    #44 Fixed setter methods in Gps class

--- src/main/java/SmartHome/domain/Gps.java
+++ src/main/java/SmartHome/domain/Gps.java
@@ -8,21 +8,19 @@ public class Gps {
         setLatitude(latitude);
         setLongitude(longitude);
     }
-    private double setLatitude(double latitude) throws IllegalArgumentException {
+    private void setLatitude(double latitude) throws IllegalArgumentException {
         if (latitude < -90 || latitude > 90) {
             throw new IllegalArgumentException("Please enter a valid latitude.");
         } else {
             this._latitude = latitude;
         }
-        return this._latitude;
     }
-    private double setLongitude(double longitude) throws IllegalArgumentException {
+    private void setLongitude(double longitude) throws IllegalArgumentException {
         if (longitude < -180 || longitude > 180) {
             throw new IllegalArgumentException("Please enter a valid longitude.");
         } else {
             this._longitude = longitude;
         }
-        return this._longitude;
     }
 
     public double getLatitude() {
