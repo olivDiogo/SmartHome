c87c9091cc13f1b6ebf46e044b0c96b6c20e079d
rueloParente <1231858@isep.ipp.pt>
2024-03-05 12:30:16 +0000

    #21 Added ElectricConsumptionWhValue.java

---
+++ src/main/java/SmartHome/sensors/ElectricConsumptionWhValue.java
@@ -0,0 +1,40 @@
+package SmartHome.sensors;
+
+import SmartHome.domain.Value;
+
+public class ElectricConsumptionWhValue implements Value, Cloneable {
+    private double _value;
+
+    public ElectricConsumptionWhValue(double value) {
+        setValue(value);
+    }
+
+    private void setValue(double value) {
+        if (value < 0)
+            throw new IllegalArgumentException("Consumption cannot be negative.");
+        else _value = value;
+    }
+    @Override
+    public String toString() {
+        return "ElectricConsumptionWh{" + +_value +
+                '}';
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        ElectricConsumptionWhValue electricConsumptionWhValue = (ElectricConsumptionWhValue) obj;
+        return electricConsumptionWhValue._value == this._value;
+    }
+    @Override
+    public ElectricConsumptionWhValue clone() {
+        try {
+            return (ElectricConsumptionWhValue) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("ElectricConsumptionWhValue instance could not be cloned.");
+        }
+    }
+
+}
