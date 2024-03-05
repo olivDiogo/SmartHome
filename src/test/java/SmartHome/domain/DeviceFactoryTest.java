91902a35dd4c41152d387c3203f0e0ded82a21ac
BernardoAmorim <1110281@isep.ipp.pt>
2024-03-05 12:05:35 +0000

    Isolation Tests for DeviceFactory

---
+++ src/test/java/SmartHome/domain/DeviceFactoryTest.java
@@ -0,0 +1,40 @@
+package SmartHome.domain;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockConstruction;
+
+/**
+ * Test suite for the {@link DeviceFactory} class.
+ */
+class DeviceFactoryTest {
+
+    /**
+     * Test to check if the constructor works.
+     */
+    @Test
+    void seeIfConstructorWorks(){
+        //Act
+        new DeviceFactory();
+    }
+
+    /**
+     * Test to check if the createDevice method returns a mocked device.
+     */
+    @Test
+    void createDeviceShouldReturnMockedDevice(){
+        //Arrange
+        String name = "Device";
+        DeviceFactory deviceFactory = new DeviceFactory();
+
+        try (MockedConstruction<Device> mockedDevice = mockConstruction(Device.class)){
+            //Act
+            Device device = deviceFactory.createDevice(name);
+
+            //Assert
+            assertTrue(mockedDevice.constructed().contains(device));
+        }
+    }
+}
