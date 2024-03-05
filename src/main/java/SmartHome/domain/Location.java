a34e4acb4e06cf3d61ae718af731d820e0230ddc
rueloParente <1231858@isep.ipp.pt>
2024-03-05 22:59:23 +0000

    #44 Cut set methods in Location, now using addressFactory and gpsFactory in constructor

--- src/main/java/SmartHome/domain/Location.java
+++ src/main/java/SmartHome/domain/Location.java
@@ -8,25 +8,14 @@ public class Location {
     protected Location(String street, String zipCode, int doorNumber, double latitude, double longitude, AddressFactory addressFactory, GpsFactory gpsFactory) throws IllegalArgumentException {
         _addressFactory = addressFactory;
         _gpsFactory = gpsFactory;
-        setAddress(street, zipCode, doorNumber);
-        setGpsLocation(latitude, longitude);
-        // are the set methods necessary?
-        //this._address = _addressFactory.createAddress(street, zipCode, doorNumber);
-        //this._gpsLocation = _gpsFactory.createGps(latitude, longitude);
+        this._address = _addressFactory.createAddress(street, zipCode, doorNumber);
+        this._gpsLocation = _gpsFactory.createGps(latitude, longitude);
     }
     
     public Address getAddress() {
         return this._address;
     }
     public Gps getGpsLocation() {
-        return this._gpsLocation;
-    }
-    private Address setAddress(String street, String zipCode, int doorNumber) {
-        this._address = _addressFactory.createAddress(street, zipCode, doorNumber);
-        return this._address;
-    }
-    private Gps setGpsLocation(double latitude, double longitude) {
-        this._gpsLocation = _gpsFactory.createGps(latitude, longitude);
         return this._gpsLocation;
     }
 
