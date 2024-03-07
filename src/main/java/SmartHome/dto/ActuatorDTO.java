0e0937492e7551c1d649658479605ced34f8294c
João Mouta <1231833@isep.ipp.pt>
2024-03-07 09:55:56 +0000

    fixed attributes inActuatorDTO

--- src/main/java/SmartHome/dto/ActuatorDTO.java
+++ src/main/java/SmartHome/dto/ActuatorDTO.java
@@ -5,7 +5,7 @@ import SmartHome.domain.ActuatorType;
 
 public class ActuatorDTO {
 
-    private final ActuatorType _actuatorModel;
+    public final ActuatorType _actuatorModel;
 
     public ActuatorDTO(Actuator actuator) {
         this._actuatorModel = actuator.getActuatorType();
