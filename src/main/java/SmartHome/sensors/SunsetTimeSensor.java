2513307e7a20aa3f9149b1b39fc0ff6349a0586d
rueloParente <1231858@isep.ipp.pt>
2024-03-07 21:25:28 +0000

    Closes #51 added javadoc documentation to SunsetTimeSensor and ElectricConsumptionWhSensor

--- src/main/java/SmartHome/sensors/SunsetTimeSensor.java
+++ src/main/java/SmartHome/sensors/SunsetTimeSensor.java
@@ -10,25 +10,42 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
-
+/**
+ * This class represents a sensor that measures the sunset time.
+ * It implements the Sensor interface and provides methods for setting the sensor type,
+ * configuring GPS location, and getting the sunset time.
+ */
 public class SunsetTimeSensor implements Sensor {
     private SensorType _sensorType;
     private double _latitude;
     private double _longitude;
     private SunsetTimeValue _sunsetTimeValue;
+    /**
+     * Constructor for the SunsetTimeSensor class.
+     * @param catalogueSensor CatalogueSensor object.
+     * @throws InstantiationException if the sensor type with description 'SunsetTime' does not exist or if the GPS configuration file is not found.
+     */
     public SunsetTimeSensor(CatalogueSensor catalogueSensor) throws InstantiationException {
     setSensorType(catalogueSensor);
     configureGpsLocation();
     }
-
-    private void setSensorType(CatalogueSensor catalogue) throws InstantiationException {
-        SensorType sensorType = catalogue.getSensorType("SunsetTime");
+    /**
+     * Method to set the sensor type.
+     * @param catalogueSensor CatalogueSensor object.
+     * @throws InstantiationException if the sensor type with description 'SunsetTime' does not exist.
+     */
+    private void setSensorType(CatalogueSensor catalogueSensor) throws InstantiationException {
+        SensorType sensorType = catalogueSensor.getSensorType("SunsetTime");
         if (sensorType == null)
             throw new InstantiationException("SensorType with description 'SunsetTime' does not exist.");
         else {
             this._sensorType = sensorType;
         }
     }
+    /**
+     * Method to configure GPS location.
+     * @throws InstantiationException if the GPS configuration file is not found.
+     */
     private void configureGpsLocation() throws InstantiationException {
         try {
             Configurations configs = new Configurations();
@@ -43,27 +60,49 @@ public class SunsetTimeSensor implements Sensor {
             throw new InstantiationException("something went wrong in reading the gps configuration: " + exception.getMessage());
         }
     }
+    /**
+     * Method to get the sunset time for the current day.
+     * @return LocalTime object representing the sunset time.
+     * @throws IllegalArgumentException if the sunset time is null.
+     */
     LocalTime getSunsetTime() throws IllegalArgumentException{
         SunTimes time = SunTimes.compute().on(LocalDate.now()).at(_latitude, _longitude).execute();
         LocalTime sunset = Objects.requireNonNull(time.getSet()).toLocalTime();
         return sunset;
     }
+    /**
+     * Method to get the sunset time for a specific date.
+     * @param date LocalDate object representing the date.
+     * @return LocalTime object representing the sunset time.
+     * @throws IllegalArgumentException if the sunset time is null.
+     */
     LocalTime getSunsetTime(LocalDate date) throws IllegalArgumentException {
         SunTimes time = SunTimes.compute().on(date).at(_latitude, _longitude).execute();
         LocalTime sunset = Objects.requireNonNull(time.getSet()).toLocalTime();
         return sunset;
     }
-
+    /**
+     * Method to get the sensor type.
+     * @return SensorType object.
+     */
     public SensorType getSensorType() {
         return this._sensorType;
     }
-
+    /**
+     * Method to get the value of the sensor for the current day.
+     * @return Value object representing the sunset time for the current day.
+     */
     //Default behavior will return sunset for current day
     public Value getValue() {
         LocalTime sunset = getSunsetTime();
         this._sunsetTimeValue = new SunsetTimeValue(sunset);
         return this._sunsetTimeValue;
     }
+    /**
+     * Method to get the value of the sensor for a specific date.
+     * @param date LocalDate object representing the date.
+     * @return Value object representing the sunset time for the specific date.
+     */
     public Value getValue(LocalDate date) {
         LocalTime sunset = getSunsetTime(date);
         this._sunsetTimeValue = new SunsetTimeValue(sunset);
