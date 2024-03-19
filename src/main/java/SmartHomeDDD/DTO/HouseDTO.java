b27eb8d11b366a5bf56cfe09c8137fa70d019cd0
João Mouta <1231833@isep.ipp.pt>
2024-03-19 15:20:11 +0000

    [UPDATE] refactor of package DTO
    Co-Authored-By: diogo-m-oliveira <147631792+diogo-m-oliveira@users.noreply.github.com>

---
+++ src/main/java/SmartHomeDDD/DTO/HouseDTO.java
@@ -0,0 +1,15 @@
+package SmartHomeDDD.DTO;
+
+import SmartHomeDDD.ValueObject.Address;
+import SmartHomeDDD.ValueObject.GPS;
+import SmartHomeDDD.ValueObject.ZipCode;
+
+public class HouseDTO  {
+    public Address _address;
+    public ZipCode _zipCode;
+
+    public GPS _gps;
+
+
+
+}
--- src/main/java/SmartHomeDDD/ddd/DTO/HouseDTO.java
+++
@@ -1,4 +0,0 @@
-package SmartHomeDDD.ddd.DTO;
-
-public class HouseDTO  {
-}
