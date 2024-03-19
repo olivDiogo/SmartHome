b9a284a701e12186feedfdc55d89912f0e555117
diogo-m-oliveira <1140500@isep.ipp.pt>
2024-03-19 16:10:56 +0000

    #149 [New]: Added class HouseID

--- src/main/java/SmartHomeDDD/ValueObject/HouseID.java
+++ src/main/java/SmartHomeDDD/ValueObject/HouseID.java
@@ -1,4 +1,57 @@
 package SmartHomeDDD.ValueObject;
 
-public class HouseID {
+import SmartHomeDDD.ddd.DomainID;
+
+public class HouseID implements DomainID {
+    private String _id;
+
+    /**
+     * Constructor for HouseID
+     * @param houseID
+     */
+    public HouseID(String houseID){
+        if (houseID != null && !houseID.isBlank() && !houseID.isEmpty())
+            this._id = houseID;
+        else
+            throw new IllegalArgumentException("Valor do parâmetro 'houseID' deve ser uma string não vazia.");
+    }
+
+    /**
+     * Getter for ID
+     * @return _id
+     */
+    public String getId() {
+        return _id;
+    }
+
+    /**
+     * Equals method for HouseID
+     *
+     * @param o Object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+
+        if (o instanceof HouseID) {
+            HouseID objectHouseId = (HouseID) o;
+
+            if (this._id.equals(objectHouseId._id))
+                return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * HashCode method for HouseID
+     *
+     * @return the hashcode as an int
+     */
+    public int hashCode(){
+        return _id.hashCode();
+    }
+
 }
--- src/main/java/SmartHomeDDD/ddd/DomainID.java
+++ src/main/java/SmartHomeDDD/ddd/DomainID.java
@@ -2,7 +2,6 @@ package SmartHomeDDD.ddd;
 
 public interface DomainID extends ValueObject {
     String getId();
-    String toString();
     boolean equals(Object o);
     int hashCode();
 
