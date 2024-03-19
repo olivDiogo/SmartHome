19bb94548bb27935ff5a46c2dd203686a80ed8df
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-19 17:07:26 +0000

    #151 [Update] Address class
    Co-Authored-By: LloydCosta87 <147655195+LloydCosta87@users.noreply.github.com>

--- src/main/java/SmartHomeDDD/ValueObject/Address.java
+++ src/main/java/SmartHomeDDD/ValueObject/Address.java
@@ -8,23 +8,23 @@ public class Address implements ValueObject {
     private String _doorNumber;
 
     public Address(String street, String doorNumber) {
-        this._street = streetValidation(street);
-        this._doorNumber = doorNumberValidation(doorNumber);
+        streetValidation(street);
+        doorNumberValidation(doorNumber);
     }
 
-    public String streetValidation(String street){
+    private void streetValidation(String street){
         if (street == null || street.trim().isEmpty() || street.length() > 70 || !street.matches("^[a-zA-Z0-9 ]+$")) {
             throw new IllegalArgumentException("Invalid street");
         } else {
-            return street;
+            this._street = street;
         }
     }
 
-    public String doorNumberValidation(String doorNumber){
-        if (doorNumber == null || doorNumber.trim().isEmpty() || doorNumber.length() > 10 || !doorNumber.matches("^[a-zA-Z0-9 ]+$")){
+    private void doorNumberValidation(String doorNumber){
+        if (doorNumber == null || doorNumber.trim().isEmpty() || doorNumber.length() > 10 || !doorNumber.matches("^[a-zA-Z0-9 ]+$")) {
             throw new IllegalArgumentException("Invalid door number");
         } else {
-            return doorNumber;
+            this._doorNumber = doorNumber;
         }
     }
 
