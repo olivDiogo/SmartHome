179e4321dc5e062a6db08f7f0ce347956aef6311
João Mouta <1231833@isep.ipp.pt>
2024-03-20 10:54:34 +0000

    #149 [CREATED] SensorTypeID class test

---
+++ src/test/java/SmartHomeDDD/ValueObject/SensorTypeIDTest.java
@@ -0,0 +1,175 @@
+package SmartHomeDDD.ValueObject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;import static org.junit.jupiter.api.Assertions.*;
+
+class SensorTypeIDTest {
+
+    /**
+     * Tests the correct instantiation of a SensorTypeID.
+     */
+    @Test
+    public void shouldGetValidObject_whenUsingValidStringInConstructor() {
+        // Arrange
+        String sensorTypeIDDescription = "Sensor2GKA";
+        // Act
+        new SensorTypeID(sensorTypeIDDescription);
+    }
+
+    /**
+     * Tests if the exception is thrown with a null sensorTypeID.
+     */
+    @Test
+    public void shouldThrowException_whenSensorTypeIdIsNull() {
+        // Arrange
+        String sensorTypeIDDescription = null;
+        String expectedMessage = "'sensorTypeID' must be a non-empty string.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new SensorTypeID(sensorTypeIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    /**
+     * Tests if the exception is thrown with a blank sensorTypeID.
+     */
+    @Test
+    public void shouldThrowException_whenSensorTypeIdIsBlank() {
+        // Arrange
+        String sensorTypeIDDescription = " ";
+        String expectedMessage = "'sensorTypeID' must be a non-empty string.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new SensorTypeID(sensorTypeIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests if the exception is thrown with an empty sensorTypeID.
+     */
+    @Test
+    public void shouldThrowException_whenSensorTypeIdIsEmpty() {
+        // Arrange
+        String sensorTypeIDDescription = "";
+        String expectedMessage = "'sensorTypeID' must be a non-empty string.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new SensorTypeID(sensorTypeIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests if the sensor type ID is returned correctly.
+     */
+    @Test
+    public void shouldReturnSensorTypeID () {
+        //Arrange
+        String sensorTypeIDDescription = "Sensor2GKA";
+        SensorTypeID sensorTypeIDObject = new SensorTypeID(sensorTypeIDDescription);
+
+        String expected = "Sensor2GKA";
+
+        //Act
+        String result = sensorTypeIDObject.getId();
+
+        //Assert
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Tests if the equals method returns true when the sensor type ID is compared to itself.
+     */
+    @Test
+    public void shouldReturnTrue_WhenSensorTypeIDIsEqualToItself() {
+        // Arrange
+        String sensorTypeIDDescription = "Sensor2GKA";
+        SensorTypeID sensorTypeID1 = new SensorTypeID(sensorTypeIDDescription);
+
+        // Act
+        boolean result = sensorTypeID1.equals(sensorTypeID1);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    /**
+     * Tests if the equals method returns true when the sensor type ID is compared to another sensor type ID with the same ID.
+     */
+    @Test
+    public void shouldReturnTrue_WhenSensorTypeIDIsEqualToOtherSensorTypeId () {
+        //Arrange
+        String sensorTypeIDDescription = "Sensor2GKA";
+        SensorTypeID sensorTypeID1 = new SensorTypeID(sensorTypeIDDescription);
+        SensorTypeID sensorTypeID2 = new SensorTypeID(sensorTypeIDDescription);
+
+        //act
+        boolean result = sensorTypeID1.equals(sensorTypeID2);
+
+        //Assert
+        assertTrue(result);
+
+    }
+
+    /**
+     * Tests if the equals method returns false when the sensor type ID is compared to another sensor type ID with a different ID.
+     */
+    @Test
+    public void shouldReturnFalse_WhenSensorTypeIDisDifferentFromOtherSensorTYpeID () {
+        //Arrange
+        String sensorTypeIDDescription = "Sensor2AVB";
+        String sensorTypeID2Description = "Sensor2PDC";
+
+        SensorTypeID sensorTypeID1 = new SensorTypeID(sensorTypeIDDescription);
+        SensorTypeID sensorTypeID2 = new SensorTypeID(sensorTypeID2Description);
+
+        //Act
+        boolean result = sensorTypeID1.equals(sensorTypeID2);
+
+        //Assert
+        assertFalse(result);
+
+    }
+
+    /**
+     * Tests if the hashcode is returned correctly.
+     */
+    @Test
+    public void shouldReturnHashCode () {
+        //Arrange
+        String sensorTypeIDDescription = "SensorAVB";
+
+        SensorTypeID sensorTypeID = new SensorTypeID(sensorTypeIDDescription);
+
+        int expected = sensorTypeIDDescription.hashCode();
+
+        //Act
+        int result = sensorTypeID.hashCode();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+}
