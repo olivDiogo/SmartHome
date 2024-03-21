c13b912f996f7f10b257b432df58a73575d51dc0
diogo-m-oliveira <1140500@isep.ipp.pt>
2024-03-21 10:24:30 +0000

    #174 [NEW]: Added SensorTypeFactory test.

---
+++ src/test/java/SmartHomeDDD/domain/SensorType/ImpSensorTypeFactoryTest.java
@@ -0,0 +1,41 @@
+package SmartHomeDDD.domain.SensorType;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import SmartHomeDDD.ValueObject.TypeDescription;
+import SmartHomeDDD.ValueObject.UnitDescription;
+
+import java.util.List;
+
+import org.mockito.MockedConstruction;
+
+public class ImpSensorTypeFactoryTest {
+
+    /**
+     * Test to ensure that a SensorType can be created successfully
+     */
+    @Test
+    public void shouldCreateSensorType_whenAttributesAreValid() {
+        // Arrange
+        TypeDescription typeDescriptionDouble = mock(TypeDescription.class);
+        UnitDescription unitDescriptionDouble = mock(UnitDescription.class);
+
+        try(MockedConstruction<SensorType> sensorTypeDouble = mockConstruction(SensorType.class,(mock, context) -> {
+            TypeDescription actualTypeDescription = (TypeDescription) context.arguments().get(0);
+            when(mock.getSensorTypeName()).thenReturn(actualTypeDescription);
+
+        })) {
+            ImpSensorTypeFactory impSensorTypeFactory = new ImpSensorTypeFactory();
+
+            // Act
+            SensorType sensorType = impSensorTypeFactory.createSensorType(typeDescriptionDouble, unitDescriptionDouble);
+
+            // Assert
+            List<SensorType> sensorTypeList = sensorTypeDouble.constructed();
+            assertEquals(1, sensorTypeList.size());
+            assertEquals(typeDescriptionDouble, sensorType.getSensorTypeName());
+        }
+    }
+}
