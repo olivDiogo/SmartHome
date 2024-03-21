3849cbd055d62d637f25bc7ee4bf929a866716ce
João Mouta <1231833@isep.ipp.pt>
2024-03-21 11:31:19 +0000

    [UPDATE] Dimension class #156
    Implemented new validations to the parameters of the class

--- src/main/java/SmartHomeDDD/ValueObject/Dimension.java
+++ src/main/java/SmartHomeDDD/ValueObject/Dimension.java
@@ -3,9 +3,9 @@ package SmartHomeDDD.ValueObject;
 import SmartHomeDDD.ddd.ValueObject;
 
 public class Dimension implements ValueObject {
-    private int _width;
-    private int _height;
-    private int _depth;
+    private final int _width;
+    private final int _height;
+    private final int _depth;
 
     /**
      * Constructor of the class Dimension.
@@ -15,9 +15,12 @@ public class Dimension implements ValueObject {
      * @param depth  is the depth of the room.
      */
     public Dimension(int width, int height, int depth) {
-        setWidth(width);
-        setHeight(height);
-        setDepth(depth);
+        validateWidth(width);
+        validateHeight(height);
+        validateDepth(depth);
+        this._width = width;
+        this._height = height;
+        this._depth = depth;
 
     }
 
@@ -27,10 +30,8 @@ public class Dimension implements ValueObject {
      * @param width is the width of the room.
      * @return the width of the room.
      */
-    private void setWidth(int width) {
-        if (width > 0)
-            this._width = width;
-        else
+    private void validateWidth(int width) {
+        if (width <= 0)
             throw new IllegalArgumentException("'Width' must be positive.");
     }
 
@@ -40,10 +41,8 @@ public class Dimension implements ValueObject {
      * @param height is the height of the room.
      * @return the height of the room.
      */
-    private void setHeight(int height) {
-        if (height > 0)
-            this._height = height;
-        else
+    private void validateHeight(int height) {
+        if (height <= 0)
             throw new IllegalArgumentException("'Height' must be positive.");
     }
 
@@ -53,10 +52,8 @@ public class Dimension implements ValueObject {
      * @param depth is the depth of the room.
      * @return the depth of the room.
      */
-    private void setDepth(int depth) {
-        if (depth > 0)
-            this._depth = depth;
-        else
+    private void validateDepth(int depth) {
+        if (depth <= 0)
             throw new IllegalArgumentException("'Depth' must be positive");
     }
 
