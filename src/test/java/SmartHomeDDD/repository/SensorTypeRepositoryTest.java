544754841c8026265617af24a55f60fa9e6e9d70
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-21 19:09:46 +0000

    #198 fixed constructor test

--- src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
+++ src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class SensorTypeRepositoryTest {
 
     @Test
-    public void shouldInstantiateSensorTypeRepository() {
+    void shouldInstantiateSensorTypeRepository() {
         new SensorTypeRepository();
     }
 }
