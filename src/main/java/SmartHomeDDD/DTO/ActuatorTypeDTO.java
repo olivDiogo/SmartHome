9073a64fbbe768cd6d3f4d6b231983771bd940ac
Sherph <1231840@isep.ipp.pt>
2024-03-21 09:54:50 +0000

    #185 Added Class ActuatorTypeDTO

---
+++ src/main/java/SmartHomeDDD/DTO/ActuatorTypeDTO.java
@@ -0,0 +1,44 @@
+package SmartHomeDDD.DTO;
+
+import SmartHomeDDD.ddd.DTO;
+
+p/**
+ * Data Transfer Object (DTO) representing an actuator type.
+ */
+public class ActuatorTypeDTO implements DTO {
+
+    /**
+     * Unique identifier for the actuator type.
+     */
+    public final String _id;
+
+    /**
+     * Description of the actuator type.
+     */
+    public final String _actuatorTypeDescription;
+
+    /**
+     * Constructs a new ActuatorTypeDTO object.
+     *
+     * @param actuatorTypeID           The unique identifier for the actuator type.
+     * @param actuatorTypeDescription  The description of the actuator type.
+     */
+    public ActuatorTypeDTO(String actuatorTypeID, String actuatorTypeDescription) {
+        this._id = actuatorTypeID;
+        this._actuatorTypeDescription = actuatorTypeDescription;
+    }
+
+    /**
+     * Returns a string representation of the ActuatorTypeDTO object.
+     *
+     * @return A string representation of the ActuatorTypeDTO object.
+     */
+    @Override
+    public String toString() {
+        return "ActuatorTypeDTO{" +
+                "_id='" + _id + '\'' +
+                ", _actuatorTypeDescription='" + _actuatorTypeDescription + '\'' +
+                '}';
+    }
+}
+
