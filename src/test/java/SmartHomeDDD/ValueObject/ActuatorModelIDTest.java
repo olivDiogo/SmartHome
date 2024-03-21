f1f11217851867cf3e265f47869a30e2176c395a
WagnerPaivaDev <1231863@isep.ipp.pt>
2024-03-21 15:52:43 +0000

    Closses #193 added ActuatorModelID  and respectives interface methodos.

---
+++ src/test/java/SmartHomeDDD/ValueObject/ActuatorModelIDTest.java
@@ -0,0 +1,123 @@
+package SmartHomeDDD.ValueObject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActuatorModelIDTest {
+
+    @Test
+    void shouldGetValidObject_whenUsingValidStringInConstructor() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        // Act
+        new ActuatorModelID(actuatorModelIDDescription);
+    }
+
+    @Test
+    void shouldThrowException_whenActuatorModelIDIsNull() {
+        // Arrange
+        String actuatorModelIDDescription = null;
+        String expectedMessage = "The value of 'actuatorModelID' should not null, blank, or empty.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new ActuatorModelID(actuatorModelIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void shouldThrowException_whenActuatorModelIDIsBlank() {
+        // Arrange
+        String actuatorModelIDDescription = " ";
+        String expectedMessage = "The value of 'actuatorModelID' should not null, blank, or empty.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new ActuatorModelID(actuatorModelIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void shouldThrowException_whenActuatorModelIDIsEmpty() {
+        // Arrange
+        String actuatorModelIDDescription = "";
+        String expectedMessage = "The value of 'actuatorModelID' should not null, blank, or empty.";
+
+        // Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new ActuatorModelID(actuatorModelIDDescription)
+        );
+
+        // Assert
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void shouldReturnActuatorTypeID() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        ActuatorModelID actuatorModelID = new ActuatorModelID(actuatorModelIDDescription);
+        // Act
+        String result = actuatorModelID.getId();
+        // Assert
+        assertEquals(actuatorModelIDDescription, result);
+    }
+
+    @Test
+    void shouldReturnTrue_WhenActuatorTypeIDIsEqualToItself() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        ActuatorModelID actuatorModelID = new ActuatorModelID(actuatorModelIDDescription);
+        // Act
+        boolean result = actuatorModelID.equals(actuatorModelID);
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnFalse_whenComparingTwoDifferentActuatorModelID() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        ActuatorModelID actuatorModelID = new ActuatorModelID(actuatorModelIDDescription);
+        ActuatorModelID actuatorModelID2 = new ActuatorModelID("light");
+        // Act
+        boolean result = actuatorModelID.equals(actuatorModelID2);
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnHashCode() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        ActuatorModelID actuatorModelID = new ActuatorModelID(actuatorModelIDDescription);
+        // Act
+        int result = actuatorModelID.hashCode();
+        // Assert
+        assertEquals(actuatorModelIDDescription.hashCode(), result);
+    }
+
+    @Test
+    void shouldReturnFalse_whenComparingActuatorModelIDToNull() {
+        // Arrange
+        String actuatorModelIDDescription = "switch";
+        ActuatorModelID actuatorModelID = new ActuatorModelID(actuatorModelIDDescription);
+        // Act
+        boolean result = actuatorModelID.equals(null);
+        // Assert
+        assertFalse(result);
+    }
+}
