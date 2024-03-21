70e5e3393ea797fc6836a464f70ba98e2f79a5c8
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-21 19:31:29 +0000

    #198 [UPDATE] SensorTypeRepo save method and tests

--- src/main/java/SmartHomeDDD/repository/SensorTypeRepository.java
+++ src/main/java/SmartHomeDDD/repository/SensorTypeRepository.java
@@ -8,7 +8,9 @@ import SmartHomeDDD.domain.SensorType.SensorType;
 
 import java.util.LinkedHashMap;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 public class SensorTypeRepository implements Repository<TypeDescription, SensorType> {
@@ -19,13 +21,30 @@ public class SensorTypeRepository implements Repository<TypeDescription, SensorT
     @Override
     public SensorType save(SensorType sensorType) {
         if (sensorType == null) {
-            throw new IllegalArgumentException("House cannot be null.");
+            throw new IllegalArgumentException("SensorType cannot be null.");
         } else if (containsOfIdentity(sensorType.getID())) {
-            throw new IllegalArgumentException("House already exists.");
+            throw new IllegalArgumentException("SensorType already exists.");
         } else {
             _sensorTypeData.put(sensorType.getID(), sensorType);
         }
         return sensorType;
+    }
+
+    @Override
+    public List<SensorType> findAll() {
+        List <SensorType> allSensorTypes = _sensorTypeData.values().stream().toList();
+        return allSensorTypes;
+    }
+
+    @Override
+    public Optional<SensorType> ofIdentity(TypeDescription typeDescription) {
+        Optional<SensorType> sensorType = Optional.ofNullable(_sensorTypeData.get(typeDescription));
+        return sensorType;
+    }
+
+    @Override
+    public boolean containsOfIdentity(TypeDescription typeDescription) {
+        return _sensorTypeData.containsKey(typeDescription);
     }
 
 }
--- src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
+++ src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
@@ -1,11 +1,65 @@
 package SmartHomeDDD.repository;
 
 import org.junit.jupiter.api.Test;
+import SmartHomeDDD.ValueObject.TypeDescription;
+import SmartHomeDDD.domain.SensorType.SensorType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SensorTypeRepositoryTest {
 
     @Test
     void shouldInstantiateSensorTypeRepository() {
         new SensorTypeRepository();
+    }
+
+    @Test
+    void shouldSaveSensorType_whenGivenValidSensorType() {
+        //Arrange
+        SensorType sensorType = mock(SensorType.class);
+        TypeDescription typeDescription = mock(TypeDescription.class);
+        when(sensorType.getID()).thenReturn(typeDescription);
+        SensorTypeRepository sensorTypeRepository = new SensorTypeRepository();
+
+        //Act
+        SensorType savedSensorType = sensorTypeRepository.save(sensorType);
+
+        //Assert
+        assertEquals(sensorType, savedSensorType);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_whenGivenNullSensorType() {
+        //Arrange
+        SensorType sensorType = null;
+        SensorTypeRepository sensorTypeRepository = new SensorTypeRepository();
+        String expectedMessage = "SensorType cannot be null.";
+
+        //Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> sensorTypeRepository.save(sensorType));
+
+        //Assert
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowException_whenSensorTypeAlreadyExists() {
+        //Arrange
+        SensorType sensorType = mock(SensorType.class);
+        TypeDescription TypeDescription = mock(TypeDescription.class);
+        when(sensorType.getID()).thenReturn(TypeDescription);
+
+        SensorTypeRepository sensorTypeRepository = new SensorTypeRepository();
+
+        sensorTypeRepository.save(sensorType);
+        String expectedMessage = "SensorType already exists.";
+
+        //Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> sensorTypeRepository.save(sensorType));
+
+        //Assert
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
