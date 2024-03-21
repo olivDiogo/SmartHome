86e56a288689de3e0a56e39f309083f636ac98de
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-21 19:36:56 +0000

    #198 [UPDATE] - findAll method and tests

--- src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
+++ src/test/java/SmartHomeDDD/repository/SensorTypeRepositoryTest.java
@@ -1,8 +1,11 @@
 package SmartHomeDDD.repository;
 
+import SmartHomeDDD.domain.House.House;
 import org.junit.jupiter.api.Test;
 import SmartHomeDDD.ValueObject.TypeDescription;
 import SmartHomeDDD.domain.SensorType.SensorType;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -61,5 +64,38 @@ public class SensorTypeRepositoryTest {
 
         //Assert
         assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    void shouldRetunrAllSensorTypes_whenFindAllIsCalled() {
+        //Arrange
+        SensorType firstSensorType = mock(SensorType.class);
+        TypeDescription firstTypeDescription = mock(TypeDescription.class);
+        when(firstSensorType.getID()).thenReturn(firstTypeDescription);
+        SensorType secondSensorType = mock(SensorType.class);
+        TypeDescription secondTypeDescription = mock(TypeDescription.class);
+        when(secondSensorType.getID()).thenReturn(secondTypeDescription);
+
+        SensorTypeRepository sensorTypeRepository = new SensorTypeRepository();
+
+        sensorTypeRepository.save(firstSensorType);
+        sensorTypeRepository.save(secondSensorType);
+        List<SensorType> expectedList = List.of(firstSensorType, secondSensorType);
+
+        //Act
+        List<SensorType> allSensorTypes = sensorTypeRepository.findAll();
+
+        //Assert
+        assertEquals(expectedList, allSensorTypes);
+    }
+
+    @Test
+    void shouldReturnEmptyList_whenNoSensorTypesAreSaved() {
+        //Arrange
+        SensorTypeRepository sensorTypeRepository = new SensorTypeRepository();
+        //Act
+        List<SensorType> allSensorTypes = sensorTypeRepository.findAll();
+        //Assert
+        assertTrue(allSensorTypes.isEmpty());
     }
 }
