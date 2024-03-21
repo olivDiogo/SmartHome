ef2927d675305abfe4758fd568ee6333abd02a8b
rueloParente <1231858@isep.ipp.pt>
2024-03-21 13:12:30 +0000

    #184 Added Measurement class and constructor

---
+++ src/main/java/SmartHomeDDD/domain/Measurement/Measurement.java
@@ -0,0 +1,28 @@
+package SmartHomeDDD.domain.Measurement;
+
+import SmartHomeDDD.ValueObject.MeasurementTypeUnit;
+import SmartHomeDDD.ValueObject.MeasurementTypeDescription;
+
+public class Measurement {
+    private MeasurementTypeUnit _measurementUnit;
+    private MeasurementTypeDescription _unitDescription;
+
+    Measurement( MeasurementTypeDescription unitDescription, MeasurementTypeUnit measurementUnit) {
+        validateMeasurementUnit(measurementUnit);
+        validateUnitDescription(unitDescription);
+        this._measurementUnit = measurementUnit;
+        this._unitDescription = unitDescription;
+    }
+    private void validateMeasurementUnit(MeasurementTypeUnit measurementUnit) {
+        if (measurementUnit == null) {
+            throw new IllegalArgumentException("Measurement unit is required");
+        }
+    }
+    private void validateUnitDescription(MeasurementTypeDescription unitDescription) {
+        if (unitDescription == null) {
+            throw new IllegalArgumentException("Unit description is required");
+        }
+    }
+
+
+}
