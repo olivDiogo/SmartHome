3bcfeebb2e2b471e57ac02df0557c1c304896920
ISEP_Switch_QA_Luis <1222637@isep.ipp.pt>
2024-03-22 11:33:19 +0000

    feat: measurement factory interface
    #197

---
+++ src/main/java/SmartHomeDDD/domain/Measurement/MeasurementFactory.java
@@ -0,0 +1,15 @@
+package SmartHomeDDD.domain.Measurement;
+
+import SmartHomeDDD.ValueObject.MeasurementTypeDescription;
+import SmartHomeDDD.ValueObject.MeasurementTypeUnit;
+
+public interface MeasurementFactory {
+
+    /**
+     * Creates and returns a new {@link MeasurementType} instance.
+     * @param measurementTypeDescription The description of the measurement type, defining its nature or category (e.g., temperature, pressure).
+     * @param measurementTypeUnit The unit of measurement (e.g., Celsius, Pascal) associated with this measurement type.
+     * @return A new {@link MeasurementType} instance configured with the specified description and unit.
+     */
+    MeasurementType createMeasurement(MeasurementTypeDescription measurementTypeDescription, MeasurementTypeUnit measurementTypeUnit);
+}
