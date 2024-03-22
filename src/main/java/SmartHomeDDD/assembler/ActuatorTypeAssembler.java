684c6807bbd86018c6430c222ed7dae1f944ee25
João Mouta <1231833@isep.ipp.pt>
2024-03-22 12:28:51 +0000

    refactor: ActuatorTypeAssembler to throw exception when a list contains or is null

--- src/main/java/SmartHomeDDD/assembler/ActuatorTypeAssembler.java
+++ src/main/java/SmartHomeDDD/assembler/ActuatorTypeAssembler.java
@@ -14,18 +14,23 @@ public class ActuatorTypeAssembler implements Assembler<ActuatorType, ActuatorTy
     /**
      * Constructor of the ActuatorTypeAssembler class.
      */
-    public ActuatorTypeAssembler () {
+    public ActuatorTypeAssembler() {
 
     }
 
     /**
      * Converts an ActuatorType domain entity to an ActuatorTypeDTO data transfer object.
+     *
      * @param actuatorType is the domain entity to be converted.
      * @return The ActuatorTypeDTO data transfer object.
      */
     @Override
     public ActuatorTypeDTO domainToDTO(ActuatorType actuatorType) {
-        String actuatorTypeDescription = actuatorTypeDescriptionToString (actuatorType.getID());
+        if (actuatorType == null) {
+            throw new IllegalArgumentException("The ActuatorType cannot be null.");
+        }
+
+        String actuatorTypeDescription = actuatorTypeDescriptionToString(actuatorType.getID());
 
         ActuatorTypeDTO actuatorTypeDTO = new ActuatorTypeDTO(actuatorTypeDescription);
         return actuatorTypeDTO;
@@ -34,32 +39,28 @@ public class ActuatorTypeAssembler implements Assembler<ActuatorType, ActuatorTy
 
     /**
      * Converts a list of ActuatorType domain entities to a list of ActuatorTypeDTO data transfer objects.
+     *
      * @param actuatorTypes is the list of domain entities to be converted.
      * @return The list of ActuatorTypeDTO data transfer objects.
      */
     @Override
     public List<ActuatorTypeDTO> domainToDTO(List<ActuatorType> actuatorTypes) {
-
-        List<ActuatorTypeDTO> actuatorTypeDTOS = new ArrayList<>();
-
-        for (ActuatorType actuatorType : actuatorTypes){
-            String actuatorTypeDescription = actuatorType.getID().toString();
-
-            ActuatorTypeDTO actuatorTypeDTO = new ActuatorTypeDTO(actuatorTypeDescription);
-            actuatorTypeDTOS.add(actuatorTypeDTO);
+        if (actuatorTypes == null || actuatorTypes.isEmpty() || actuatorTypes.contains(null)) {
+            throw new IllegalArgumentException("The list of ActuatorTypes cannot be null.");
         }
+        List<ActuatorTypeDTO> actuatorTypeDTOS = actuatorTypes.stream().map(this::domainToDTO).toList();
         return actuatorTypeDTOS;
     }
 
     /**
      * Converts an ActuatorTypeDTO data transfer object to an ActuatorType domain entity.
+     *
      * @param typeDescription is the data transfer object to be converted.
      * @return The ActuatorType domain entity.
      */
-    public String actuatorTypeDescriptionToString (TypeDescription typeDescription){
+    public String actuatorTypeDescriptionToString(TypeDescription typeDescription) {
         return typeDescription.toString();
     }
-
 
 
 }
