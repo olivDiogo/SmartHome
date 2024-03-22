6dfe9cc72ccedf62f3a1eb2fb50305bbe2c98eff
simao-campos87 <1231859@isep.ipp.pt>
2024-03-22 17:35:19 +0000

    #169 [update] tests with new defined unit.

--- src/test/java/SmartHomeDDD/assembler/HouseAssemblerTest.java
+++ src/test/java/SmartHomeDDD/assembler/HouseAssemblerTest.java
@@ -21,8 +21,12 @@ public class HouseAssemblerTest {
         new HouseAssembler();
     }
 
+
+    /**
+     * Test that the HouseAssembler class can convert a House object to a HouseDTO object.
+     */
     @Test
-    public void shouldReturnAHouseDTOWhenGivenAHouse() {
+    public void houldReturnAHouseDTOWhenGivenAHouse() {
         // Arrange
         House house = mock(House.class);
         when(house.getZipCode()).thenReturn(mock(ZipCode.class));
@@ -32,33 +36,20 @@ public class HouseAssemblerTest {
         when(house.getGps()).thenReturn(mock(GPS.class));
         when(house.getGps().toString()).thenReturn("GPS{latitude=90.0, longitude=180.0}");
 
-
         HouseAssembler houseAssembler = new HouseAssembler();
 
-        try (MockedConstruction<HouseDTO> mocked = mockConstruction(HouseDTO.class, (mock, context) -> {
-            String strAddress = (String) context.arguments().get(0);
-            String strPostalCode = (String) context.arguments().get(1);
-            String strGPS = (String) context.arguments().get(2);
+        // Act
+        HouseDTO result = houseAssembler.domainToDTO(house);
 
-            when(mock.getGPS()).thenReturn(strGPS);
-            when(mock.getAddress()).thenReturn(strAddress);
-            when(mock.getPostalCode()).thenReturn(strPostalCode);
-        })) {
-            // Act
-            HouseDTO result = houseAssembler.domainToDTO(house);
-            String address = result.getAddress();
-            String postalCode = result.getPostalCode();
-            String gps = result.getGPS();
-
-            // Assert
-            List<HouseDTO> houseDTOList = mocked.constructed();
-            assertEquals(address, "Test Address, 1");
-            assertEquals(postalCode, "1234-100");
-            assertEquals(gps, "GPS{latitude=90.0, longitude=180.0}");
-            assertEquals(result.toString(), houseDTOList.get(0).toString());
-        }
+        // Assert
+        assertEquals(result.address, "Test Address, 1");
+        assertEquals(result.zipCode, "1234-100");
+        assertEquals(result.gps, "GPS{latitude=90.0, longitude=180.0}");
     }
 
+    /**
+     * Test that the HouseAssembler class can convert a House object list to a HouseDTO object list.
+     */
     @Test
     public void shouldReturnANewHouseDTOListWhenGivenAListOfHouses() {
         // Arrange
@@ -84,19 +75,16 @@ public class HouseAssemblerTest {
 
         HouseAssembler houseAssembler = new HouseAssembler();
 
-        try (MockedConstruction<HouseDTO> mocked = mockConstruction(HouseDTO.class, (mock, context) -> {
+        // Act
+        List<HouseDTO> result = houseAssembler.domainToDTO(houses);
 
-        })) {
-            // Act
-            List<HouseDTO> result = houseAssembler.domainToDTO(houses);
-
-            // Assert
-            List<HouseDTO> houseDTOList = mocked.constructed();
-            assertArrayEquals(result.toArray(), houseDTOList.toArray());
-            assertEquals(result.toString(), houseDTOList.toString());
-            assertTrue(result.containsAll(houseDTOList));
-        }
+        // Assert
+        assertEquals(result.get(0).address, "Test Address, 1");
+        assertEquals(result.get(0).zipCode, "1234-100");
+        assertEquals(result.get(0).gps, "GPS{latitude=90.0, longitude=180.0}");
+        assertEquals(result.get(1).address, "Test Address2, 2");
+        assertEquals(result.get(1).zipCode, "1234-101");
+        assertEquals(result.get(1).gps, "GPS{latitude=90.0, longitude=170.0}");
     }
-
 }
 
