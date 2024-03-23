06ac68896c7223803988a3328ba6a83184a93872
João Mouta <1231833@isep.ipp.pt>
2024-03-23 07:08:00 +0000

    new: tested and implemented ActuatorTypeRepository #202

---
+++ src/main/java/SmartHomeDDD/repository/ActuatorTypeRepository.java
@@ -0,0 +1,67 @@
+package SmartHomeDDD.repository;
+
+import SmartHomeDDD.ValueObject.ActuatorTypeID;
+import SmartHomeDDD.ValueObject.TypeDescription;
+import SmartHomeDDD.ddd.Repository;
+import SmartHomeDDD.domain.ActuatorType.ActuatorType;
+
+import java.util.*;
+
+public class ActuatorTypeRepository implements Repository<ActuatorTypeID, ActuatorType> {
+
+    private final Map<ActuatorTypeID, ActuatorType> _actuatorTypeData = new LinkedHashMap<>();
+
+    /**
+     * Save an ActuatorType. If the ActuatorType is null, throw an IllegalArgumentException.
+     *
+     * @param actuatorType is the domain entity to be saved.
+     * @return the saved ActuatorType.
+     */
+    @Override
+    public ActuatorType save(ActuatorType actuatorType) {
+        if (actuatorType == null) {
+            throw new IllegalArgumentException("ActuatorType cannot be null.");
+        } else if (containsOfIdentity(actuatorType.getID())) {
+            throw new IllegalArgumentException("ActuatorType already exists.");
+        } else {
+            _actuatorTypeData.put(actuatorType.getID(), actuatorType);
+        }
+        return actuatorType;
+    }
+
+    /**
+     * Find all ActuatorTypes.
+     *
+     * @return a list of all ActuatorTypes.
+     */
+    @Override
+    public List<ActuatorType> findAll() {
+        List<ActuatorType> allActuatorTypes = _actuatorTypeData.values().stream().toList();
+
+        return allActuatorTypes;
+    }
+
+    /**
+     * Find an ActuatorType by its identity.
+     *
+     * @param actuatorTypeID is the unique identifier of the domain entity.
+     * @return the ActuatorType if found, otherwise Optional.empty().
+     */
+    @Override
+    public Optional<ActuatorType> ofIdentity(ActuatorTypeID actuatorTypeID) {
+        Optional<ActuatorType> actuatorType = Optional.ofNullable(_actuatorTypeData.get(actuatorTypeID));
+
+        return actuatorType;
+    }
+
+    /**
+     * Checks if the repository contains an ActuatorType with the given identity.
+     *
+     * @param actuatorTypeID is the unique identifier of the domain entity.
+     * @return true if the repository contains the ActuatorType, otherwise false.
+     */
+    @Override
+    public boolean containsOfIdentity(ActuatorTypeID actuatorTypeID) {
+        return _actuatorTypeData.containsKey(actuatorTypeID);
+    }
+}
---
+++ src/test/java/SmartHomeDDD/repository/ActuatorTypeRepositoryTest.java
@@ -0,0 +1,203 @@
+package SmartHomeDDD.repository;
+
+import SmartHomeDDD.ValueObject.ActuatorTypeID;
+import SmartHomeDDD.ValueObject.TypeDescription;
+import SmartHomeDDD.domain.ActuatorType.ActuatorType;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ActuatorTypeRepositoryTest {
+
+    /**
+     * Test of ActuatorTypeRepository constructor.
+     */
+    @Test
+    public void shouldInstantiateActuatorTypeRepository() {
+        new ActuatorTypeRepository();
+    }
+
+    /**
+     * Test the save method with valid ActuatorType.
+     */
+    @Test
+    public void shouldSaveActuatorType_whenGivenValidActuatorType() {
+        //Arrange
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        //Act
+        ActuatorType savedActuatorType = actuatorTypeRepository.save(actuatorType);
+
+        //Assert
+        assertEquals(actuatorType, savedActuatorType);
+    }
+
+    /**
+     * Test the save method with null ActuatorType.
+     */
+    @Test
+    public void shouldThrowIllegalArgumentException_whenGivenNullActuatorType() {
+        //Arrange
+        ActuatorType actuatorType = null;
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        String expectedMessage = "ActuatorType cannot be null.";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            //Act
+            actuatorTypeRepository.save(actuatorType);
+        });
+
+        String actualMessage = exception.getMessage();
+
+        //Assert
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    /**
+     * Test the save method with an ActuatorType that already exists.
+     */
+    @Test
+    public void shouldThrowException_WhenSActuatorTypeAlreadyExists() {
+        //Arrange
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        actuatorTypeRepository.save(actuatorType);
+
+        String expectedMessage = "ActuatorType already exists.";
+
+        //Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            actuatorTypeRepository.save(actuatorType);
+        });
+
+        String actualMessage = exception.getMessage();
+
+        //Assert
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    /**
+     * Tests if all the actuators type are returned correctly.
+     */
+    @Test
+    public void shouldReturnAllActuatorTypes() {
+        //Arrange
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+
+        ActuatorType secondActuatorType = mock(ActuatorType.class);
+        ActuatorTypeID secondActuatorTypeID = mock(ActuatorTypeID.class);
+        when(secondActuatorType.getID()).thenReturn(secondActuatorTypeID);
+
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        actuatorTypeRepository.save(actuatorType);
+        actuatorTypeRepository.save(secondActuatorType);
+        List<ActuatorType> expectedList = List.of(actuatorType, secondActuatorType);
+
+        //Act
+        List<ActuatorType> allActuatorTypes = actuatorTypeRepository.findAll();
+
+
+        //Assert
+        assertEquals(expectedList, allActuatorTypes);
+    }
+
+    /**
+     * Test the findAll method when there are no ActuatorTypes saved.
+     */
+    @Test
+    public void shouldReturnEmptyList_whenNoActuatorTypesSaved() {
+        //Arrange
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        //Act
+        List<ActuatorType> allActuatorTypes = actuatorTypeRepository.findAll();
+
+        //Assert
+        assertTrue(allActuatorTypes.isEmpty());
+    }
+
+    /**
+     * Test the ofIdentity method with an invalid ActuatorTypeID.
+     */
+    @Test
+    public void shouldReturnOptionalEmpty_whenGivenInvalidActuatorTypeID() {
+        //Arrange
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+
+        actuatorTypeRepository.save(actuatorType);
+
+        ActuatorTypeID invalidActuatorTypeID = mock(ActuatorTypeID.class);
+
+        //Act
+        Optional<ActuatorType> returnedActuatorType = actuatorTypeRepository.ofIdentity(invalidActuatorTypeID);
+
+        //Assert
+        assertTrue(returnedActuatorType.isEmpty());
+    }
+
+    /**
+     * Test the ofIdentity method with a valid ActuatorTypeID.
+     */
+    @Test
+    public void shouldReturnTrue_WhenGivenValidActuatorTypeID() {
+        //Arrange
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+        actuatorTypeRepository.save(actuatorType);
+
+        //Act
+        boolean containsActuatorType = actuatorTypeRepository.containsOfIdentity(actuatorTypeID);
+
+        //Assert
+        assertTrue(containsActuatorType);
+    }
+
+    /**
+     * Test the ofIdentity method with invalid ActuatorTypeID.
+     */
+    @Test
+    public void shouldReturnFalse_whenGivenInvalidActuatorTypeID() {
+        //Arrange
+        ActuatorTypeRepository actuatorTypeRepository = new ActuatorTypeRepository();
+
+        ActuatorType actuatorType = mock(ActuatorType.class);
+        ActuatorTypeID actuatorTypeID = mock(ActuatorTypeID.class);
+        when(actuatorType.getID()).thenReturn(actuatorTypeID);
+
+        actuatorTypeRepository.save(actuatorType);
+
+        ActuatorTypeID invalidActuatorTypeID = mock(ActuatorTypeID.class);
+
+        //Act
+        boolean containsActuatorType = actuatorTypeRepository.containsOfIdentity(invalidActuatorTypeID);
+
+        //Assert
+        assertFalse(containsActuatorType);
+    }
+
+
+}
