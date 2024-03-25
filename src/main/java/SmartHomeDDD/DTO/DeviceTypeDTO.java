bd5a26a4b0c695e9bbe747cecaff67a1f6ea00fd
simao-campos87 <1231859@isep.ipp.pt>
2024-03-25 21:48:45 +0000

    #230 [new] added DeviceTypeDTO class

---
+++ src/main/java/SmartHomeDDD/DTO/DeviceTypeDTO.java
@@ -0,0 +1,31 @@
+package SmartHomeDDD.DTO;
+
+import SmartHomeDDD.ddd.DTO;
+
+public class DeviceTypeDTO implements DTO {
+
+    public final String deviceTypeID;
+    public final String deviceTypeDescription;
+
+    /**
+     * Constructs a new DeviceTypeDTO object.
+     *
+     * @param deviceTypeID          The description of the device type.
+     * @param deviceTypeDescription The description of the device type.
+     */
+    public DeviceTypeDTO(String deviceTypeID, String deviceTypeDescription) {
+        this.deviceTypeID = deviceTypeID;
+        this.deviceTypeDescription = deviceTypeDescription;
+    }
+
+    /**
+     * Returns a string representation of the DeviceTypeDTO object.
+     */
+    @Override
+    public String toString() {
+        return "DeviceTypeDTO{" +
+                "deviceTypeID='" + deviceTypeID + '\'' +
+                ", deviceTypeDescription='" + deviceTypeDescription + '\'' +
+                '}';
+    }
+}
