621897c659e2916a4ce6c4010fe32d53755e3a76
João Mouta <1231833@isep.ipp.pt>
2024-03-26 16:56:46 +0000

    new: implemented GetDevicesFromRoomController and respective tests

---
+++ src/test/java/SmartHomeDDD/controller/GetDevicesFromRoomControllerTest.java
@@ -0,0 +1,259 @@
+package SmartHomeDDD.controller;
+
+import SmartHomeDDD.DTO.DeviceDTO;
+import SmartHomeDDD.DTO.RoomDTO;
+import SmartHomeDDD.assembler.DeviceAssembler;
+import SmartHomeDDD.assembler.RoomAssembler;
+import SmartHomeDDD.domain.Device.Device;
+import SmartHomeDDD.domain.Device.ImpDeviceFactory;
+import SmartHomeDDD.domain.House.House;
+import SmartHomeDDD.domain.House.ImpHouseFactory;
+import SmartHomeDDD.domain.Room.ImpRoomFactory;
+import SmartHomeDDD.domain.Room.Room;
+import SmartHomeDDD.domain.Room.RoomFactory;
+import SmartHomeDDD.repository.DeviceRepository;
+import SmartHomeDDD.repository.HouseRepository;
+import SmartHomeDDD.repository.RoomRepository;
+import SmartHomeDDD.service.DeviceService;
+import SmartHomeDDD.service.HouseService;
+import SmartHomeDDD.service.RoomService;
+import SmartHomeDDD.valueObject.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetDevicesFromRoomControllerTest {
+
+    /**
+     * Test to check if the GetDevicesFromRoomController constructor instantiates the controller correctly.
+     */
+    @Test
+    public void shouldInstantiateGetDevicesFromRoomController_WhenParametersAreValid() {
+        //Arrange
+        RoomRepository roomRepository = new RoomRepository();
+        ImpRoomFactory roomFactory = new ImpRoomFactory();
+        RoomAssembler roomAssembler = new RoomAssembler();
+        HouseRepository houseRepository = new HouseRepository();
+
+        RoomService roomService = new RoomService(roomRepository, roomFactory, roomAssembler, houseRepository);
+
+        DeviceRepository deviceRepository = new DeviceRepository();
+        ImpDeviceFactory deviceFactory = new ImpDeviceFactory();
+
+        DeviceService deviceService = new DeviceService(deviceRepository, deviceFactory, roomRepository);
+
+        DeviceAssembler deviceAssembler = new DeviceAssembler();
+
+        //Act
+        new GetDevicesFromRoomController(roomService, deviceService, roomAssembler, deviceAssembler);
+
+    }
+
+    /**
+     * Test to get rooms from a house.
+     */
+    @Test
+    public void shouldGetRoomsFromHouse_WhenGivenValidHouseID () {
+        //Arrange
+        RoomRepository roomRepository = new RoomRepository();
+        ImpRoomFactory roomFactory = new ImpRoomFactory();
+        RoomAssembler roomAssembler = new RoomAssembler();
+        HouseRepository houseRepository = new HouseRepository();
+        ImpHouseFactory houseFactory = new ImpHouseFactory();
+
+        HouseService houseService = new HouseService(houseFactory, houseRepository);
+        RoomService roomService = new RoomService(roomRepository, roomFactory, roomAssembler, houseRepository);
+
+        DeviceRepository deviceRepository = new DeviceRepository();
+        ImpDeviceFactory deviceFactory = new ImpDeviceFactory();
+
+        DeviceService deviceService = new DeviceService(deviceRepository, deviceFactory, roomRepository);
+        DeviceAssembler deviceAssembler = new DeviceAssembler();
+
+        GetDevicesFromRoomController getDevicesFromRoomController = new GetDevicesFromRoomController(roomService, deviceService, roomAssembler, deviceAssembler);
+
+        String street = "Rua Do Isep";
+        String doorNumber = "122A";
+        Address newAddress = new Address(street, doorNumber);
+
+        int zipCodePrefix = 4000;
+        int zipCodeSuffix = 129;
+        ZipCode newZipCode = new ZipCode(zipCodePrefix, zipCodeSuffix);
+
+        double latitude = 41.178;
+        double longitude = -8.608;
+        GPS newGPS = new GPS(latitude, longitude);
+
+        House house = houseService.addHouse(newAddress, newZipCode, newGPS);
+
+        HouseID houseID = house.getID();
+        String name2 = "Quarto da Maria";
+        RoomName roomName2 = new RoomName(name2);
+        String name1 = "Quarto do Joao";
+        RoomName roomName1 = new RoomName(name1);
+
+        int width = 10;
+        int length = 10;
+        int height = 10;
+        Dimension dimension = new Dimension(width, length, height);
+
+        int floor = 2;
+        RoomFloor roomFloor = new RoomFloor(floor);
+
+        roomService.addRoom(houseID, roomName1, dimension, roomFloor);
+        roomService.addRoom(houseID, roomName2, dimension, roomFloor);
+
+        List<Room> rooms = roomRepository.findAll();
+
+        List<RoomDTO> expectedRoomsDTOList = roomAssembler.domainToDTO(rooms);
+        String expectedRoomDTOName1 = expectedRoomsDTOList.get(0).roomName;
+        String expectedRoomDTOID1 = expectedRoomsDTOList.get(0).roomId;
+        String expectedRoomDTOName2 = expectedRoomsDTOList.get(1).roomName;
+        String expectedRoomDTOID2 = expectedRoomsDTOList.get(1).roomId;
+
+        List<String> expectedList = List.of(expectedRoomDTOName1, expectedRoomDTOID1, expectedRoomDTOName2, expectedRoomDTOID2);
+
+        //Act
+        List<RoomDTO> roomsDTOList = getDevicesFromRoomController.getRooms();
+
+        String actualRoomDTOName1 = roomsDTOList.get(0).roomName;
+        String actualRoomDTOID1 = roomsDTOList.get(0).roomId;
+        String actualRoomDTOName2 = roomsDTOList.get(1).roomName;
+        String actualRoomDTOID2 = roomsDTOList.get(1).roomId;
+
+        List<String> actualList = List.of(actualRoomDTOName1, actualRoomDTOID1, actualRoomDTOName2, actualRoomDTOID2);
+
+        //Assert
+        assertEquals(expectedList, actualList);
+
+    }
+
+    /**
+     * Test to throw an exception when the room ID does not exist in the repository.
+     */
+    @Test
+    public void shouldThrowException_WhenRoomIDDoesNotExistInRepository() {
+        // Arrange
+        RoomRepository roomRepository = new RoomRepository();
+        ImpRoomFactory roomFactory = new ImpRoomFactory();
+        RoomAssembler roomAssembler = new RoomAssembler();
+        HouseRepository houseRepository = new HouseRepository();
+        ImpHouseFactory houseFactory = new ImpHouseFactory();
+        HouseService houseService = new HouseService(houseFactory, houseRepository);
+
+        RoomService roomService = new RoomService(roomRepository, roomFactory, roomAssembler, houseRepository);
+
+        DeviceRepository deviceRepository = new DeviceRepository();
+        ImpDeviceFactory deviceFactory = new ImpDeviceFactory();
+
+        DeviceService deviceService = new DeviceService(deviceRepository, deviceFactory, roomRepository);
+        DeviceAssembler deviceAssembler = new DeviceAssembler();
+
+        GetDevicesFromRoomController getDevicesFromRoomController = new GetDevicesFromRoomController(roomService, deviceService, roomAssembler, deviceAssembler);
+
+        String roomID = "123";
+        RoomID nonExistentRoomID = new RoomID(roomID);
+
+        String roomName = "Quarto da Maria";
+        String dimension = "10x10x10";
+        String floor = "2";
+        RoomDTO roomDTO = new RoomDTO(roomName, dimension, floor, nonExistentRoomID.toString());
+
+        String expectedMessage = "Room with ID " + nonExistentRoomID + " not found.";
+
+        // Act & Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> getDevicesFromRoomController.getDevicesFromRoom(roomDTO));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    /**
+     * Test to get devices from a room.
+     */
+    @Test
+    public void shouldGetDevicesFromRoom_WhenParametersAreValid() {
+        //Arrange
+        RoomRepository roomRepository = new RoomRepository();
+        ImpRoomFactory roomFactory = new ImpRoomFactory();
+        RoomAssembler roomAssembler = new RoomAssembler();
+        DeviceAssembler deviceAssembler = new DeviceAssembler();
+        HouseRepository houseRepository = new HouseRepository();
+        ImpHouseFactory houseFactory = new ImpHouseFactory();
+        HouseService houseService = new HouseService(houseFactory, houseRepository);
+
+        DeviceRepository deviceRepository = new DeviceRepository();
+        ImpDeviceFactory deviceFactory = new ImpDeviceFactory();
+
+        RoomService roomService = new RoomService(roomRepository, roomFactory, roomAssembler, houseRepository);
+        DeviceService deviceService = new DeviceService(deviceRepository, deviceFactory, roomRepository);
+
+        GetDevicesFromRoomController getDevicesFromRoomController = new GetDevicesFromRoomController(roomService, deviceService, roomAssembler, deviceAssembler);
+
+        String street = "Rua Do Isep";
+        String doorNumber = "122A";
+        Address newAddress = new Address(street, doorNumber);
+
+        int zipCodePrefix = 4000;
+        int zipCodeSuffix = 129;
+        ZipCode newZipCode = new ZipCode(zipCodePrefix, zipCodeSuffix);
+
+        double latitude = 41.178;
+        double longitude = -8.608;
+        GPS newGPS = new GPS(latitude, longitude);
+
+        House house = houseService.addHouse(newAddress, newZipCode, newGPS);
+
+        HouseID houseID = house.getID();
+        String name2 = "Quarto da Maria";
+        int width = 10;
+        int length = 10;
+        int height = 10;
+        int floor = 2;
+
+        RoomName roomName2 = new RoomName(name2);
+        RoomFloor roomFloor = new RoomFloor(floor);
+        Dimension dimension = new Dimension(width, length, height);
+
+        Room room2 = roomService.addRoom(houseID, roomName2, dimension, roomFloor);
+
+        RoomID roomID = room2.getID();
+        String name1 = "Lampada";
+        String nameDevice = "Ar Condicionado";
+        DeviceName deviceName = new DeviceName(name1);
+        DeviceName deviceName2 = new DeviceName(nameDevice);
+        DeviceStatus deviceStatus = new DeviceStatus(true);
+
+        deviceService.addDevice(roomID, deviceName, deviceStatus);
+        deviceService.addDevice(roomID, deviceName2, deviceStatus);
+
+        List<RoomDTO> roomsDTOList = getDevicesFromRoomController.getRooms();
+        RoomDTO roomDTO = roomsDTOList.get(0);
+
+        List<Device> devices = deviceService.getDevicesByRoomId(roomID);
+        List<DeviceDTO> deviceDTOListExpected = deviceAssembler.domainToDTO(devices);
+
+        String expectedDeviceDTOID = deviceDTOListExpected.get(0).deviceID;
+        String expectedDeviceDTOName = deviceDTOListExpected.get(0).deviceName;
+        String expectedDeviceDTOID2 = deviceDTOListExpected.get(1).deviceID;
+        String expectedDeviceDTOName2 = deviceDTOListExpected.get(1).deviceName;
+
+        List<String> expectedDeviceDTOList = List.of(expectedDeviceDTOID, expectedDeviceDTOName, expectedDeviceDTOID2, expectedDeviceDTOName2);
+
+        //Act
+        List<DeviceDTO> devicesDTOList = getDevicesFromRoomController.getDevicesFromRoom(roomDTO);
+
+        String actualDeviceDTOID = devicesDTOList.get(0).deviceID;
+        String actualDeviceDTOName = devicesDTOList.get(0).deviceName;
+        String actualDeviceDTOID2 = devicesDTOList.get(1).deviceID;
+        String actualDeviceDTOName2 = devicesDTOList.get(1).deviceName;
+
+        List<String> actualDeviceDTOList = List.of(actualDeviceDTOID, actualDeviceDTOName, actualDeviceDTOID2, actualDeviceDTOName2);
+
+        //Assert
+        assertEquals(expectedDeviceDTOList, actualDeviceDTOList);
+    }
+
+
+}
