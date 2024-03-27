e66973b1da8d09ef8b3c54959737da04505898f2
Lloyd <1231838@isep.ipp.pt>
2024-03-27 12:02:08 +0000

    #237 [NEW] - implementation of SensorModelDTO, closes #237

---
+++ src/main/java/SmartHomeDDD/DTO/SensorModelDTO.java
@@ -0,0 +1,36 @@
+package SmartHomeDDD.DTO;
+
+import SmartHomeDDD.ddd.DTO;
+
+/**
+ * Represents a Data Transfer Object (DTO) for a sensor model.
+ */
+public final class SensorModelDTO implements DTO {
+    /**
+     * The unique identifier of the sensor model.
+     */
+    public final String _id;
+
+    /**
+     * The name of the sensor model.
+     */
+    public final String _name;
+
+    /**
+     * The file path to the sensor model's data.
+     */
+    public final String _modelPath;
+
+    /**
+     * Constructs a new {@code SensorModelDTO} with the specified ID, name, and model path.
+     *
+     * @param sensorModelID the unique identifier of the sensor model; should not be {@code null}.
+     * @param sensorModelName the name of the sensor model; should not be {@code null}.
+     * @param modelPath the file path to the sensor model's data; should not be {@code null}.
+     */
+    public SensorModelDTO(String sensorModelID, String sensorModelName, String modelPath) {
+        this._id = sensorModelID;
+        this._name = sensorModelName;
+        this._modelPath = modelPath;
+    }
+}
