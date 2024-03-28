22567d51967278e0302c2b86d289c4a0ac040f72
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-28 15:50:52 +0000

    #235 refactor: DeviceTypeService methods and Tests

--- src/main/java/SmartHomeDDD/service/DeviceTypeService.java
+++ src/main/java/SmartHomeDDD/service/DeviceTypeService.java
@@ -1,6 +1,5 @@
 package SmartHomeDDD.service;
 
-import SmartHomeDDD.assembler.DeviceAssembler;
 import SmartHomeDDD.domain.DeviceType.DeviceType;
 import SmartHomeDDD.domain.DeviceType.DeviceTypeFactory;
 import SmartHomeDDD.repository.DeviceTypeRepository;
@@ -14,11 +13,21 @@ public class DeviceTypeService {
 
     private DeviceTypeRepository _deviceTypeRepository;
     private DeviceTypeFactory _deviceTypeFactory;
+
+    /**
+     * Constructor for the DeviceTypeService class.
+     * @param deviceTypeRepository The repository for the device type.
+     * @param deviceTypeFactory The factory for the device type.
+     */
     public DeviceTypeService(DeviceTypeRepository deviceTypeRepository, DeviceTypeFactory deviceTypeFactory) {
         validateDeviceTypeRepository(deviceTypeRepository);
         validateDeviceTypeFactory(deviceTypeFactory);
     }
 
+    /**
+     * Validates the device type repository.
+     * @param deviceTypeRepository The repository for the device type.
+     */
     private void validateDeviceTypeRepository(DeviceTypeRepository deviceTypeRepository) {
         if (deviceTypeRepository == null) {
             throw new IllegalArgumentException("Please enter a valid device type repository.");
@@ -27,6 +36,11 @@ public class DeviceTypeService {
         }
     }
 
+
+    /**
+     * Validates the device type factory.
+     * @param deviceTypeFactory The factory for the device type.
+     */
     private void validateDeviceTypeFactory(DeviceTypeFactory deviceTypeFactory) {
         if (deviceTypeFactory == null) {
             throw new IllegalArgumentException("Please enter a valid device type factory.");
@@ -36,28 +50,40 @@ public class DeviceTypeService {
     }
 
 
-    public DeviceType createDeviceType(TypeDescription typeDescription) {
-        DeviceType deviceType = _deviceTypeFactory.createDeviceType(typeDescription);
-        return deviceType;
-    }
-
-    public DeviceType saveDeviceType(DeviceType deviceType) {
-        if (deviceType == null) {
+    /**
+     * Adds a device type.
+     * @param deviceTypeName The name of the device type.
+     * @return The device type.
+     */
+    public DeviceType addDeviceType(TypeDescription deviceTypeName) {
+        if (deviceTypeName == null) {
             throw new IllegalArgumentException("Please enter a valid device type.");
         }
-        return _deviceTypeRepository.save(deviceType);
+        DeviceType deviceType = _deviceTypeFactory.createDeviceType(deviceTypeName);
+        _deviceTypeRepository.save(deviceType);
+
+        return deviceType;
+
     }
 
+    /**
+     * Finds all device types.
+     * @return A list of all device types.
+     */
     public List<DeviceType> findAllDeviceTypes() {
         return _deviceTypeRepository.findAll();
     }
 
+    /**
+     * Finds a device type by its ID.
+     * @param deviceTypeID The ID of the device type.
+     * @return The device type.
+     */
     public Optional<DeviceType> getDeviceTypeByID(DeviceTypeID deviceTypeID) {
         if (deviceTypeID == null) {
             throw new IllegalArgumentException("Please enter a valid device type ID.");
         }
         return _deviceTypeRepository.ofIdentity(deviceTypeID);
     }
-
 
 }
