550e633fd16c1e0a34d838e522d662bcde4ff2e3
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-28 12:17:39 +0000

    #179 feat: Interface Actuator methods

---
+++ src/main/java/SmartHomeDDD/domain/Actuator/Actuator.java
@@ -0,0 +1,53 @@
+package SmartHomeDDD.domain.Actuator;
+
+import SmartHomeDDD.ddd.AggregateRoot;
+import SmartHomeDDD.ddd.ValueObject;
+import SmartHomeDDD.valueObject.ActuatorID;
+import SmartHomeDDD.valueObject.ActuatorName;
+import SmartHomeDDD.valueObject.ActuatorTypeID;
+import SmartHomeDDD.valueObject.ModelPath;
+
+public interface Actuator extends AggregateRoot<ActuatorID> {
+
+    /**
+     * Gets the actuator ID.
+     *
+     * @return The actuator ID.
+     */
+    public ActuatorID getID();
+
+    /**
+     * Gets the actuator name.
+     *
+     * @return The actuator name.
+     */
+    public ActuatorName getName();
+
+    /**
+     * Gets the model path.
+     *
+     * @return The model path.
+     */
+    public ModelPath getModelPath();
+
+    /**
+     * Gets the actuator type ID.
+     *
+     * @return The actuator type ID.
+     */
+    public ActuatorTypeID getActuatorTypeID();
+
+    /**     * Returns the actuator attributes in a string format.
+     *
+     * @return The actuator attributes in a string format.
+     */
+    public String toString();
+
+    /**
+     * Method to get the value object of the actuator.
+     * @return the value.
+     */
+    public ValueObject setValue (ValueObject value);
+
+
+}
