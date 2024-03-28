0a5e362e7b55bcc95ec883312679bf70558fea22
diogo-m-oliveira <1140500@isep.ipp.pt>
2024-03-28 16:56:55 +0000

    #262 feat: added classes SetIntegerActuator and SetIntegerValue, and respective test class (SetIntegerActuator contains SetIntegerValue).

---
+++ src/test/java/SmartHomeDDD/domain/Actuator/SetIntegerValueTest.java
@@ -0,0 +1,32 @@
+package SmartHomeDDD.domain.Actuator;
+
+import org.junit.jupiter.api.Test;
+
+public class SetIntegerValueTest {
+    /**
+     * Test for SetIntegerValue instantiation
+     */
+    @Test
+    public void shouldInstantiateSetIntegerValue() {
+        // Act
+        new SetIntegerValue(1);
+    }
+
+    /**
+     * Test for method toString
+     */
+    @Test
+    public void shouldReturnStringValue() {
+        // Arrange
+        int value = 1;
+        SetIntegerValue setIntegerValue = new SetIntegerValue(value);
+
+        String expected = "1";
+
+        // Act
+        String result = setIntegerValue.toString();
+
+        // Assert
+        assert(result.equals(expected));
+    }
+}
