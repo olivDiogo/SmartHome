96c181fac38a1b1d9f365c19b8a22db6b59c983c
WagnerPaivaDev <1231863@isep.ipp.pt>
2024-04-02 14:15:02 +0100

    Closes #232 Add DeviceTypeAssembler and Testes.

---
+++ src/main/java/SmartHomeDDD/assembler/DeviceTypeAssembler.java
@@ -0,0 +1,72 @@
+package SmartHomeDDD.assembler;
+
+
+import SmartHomeDDD.DTO.DeviceTypeDTO;
+import SmartHomeDDD.ddd.Assembler;
+import SmartHomeDDD.domain.DeviceType.DeviceType;
+import SmartHomeDDD.valueObject.DeviceTypeID;
+import SmartHomeDDD.valueObject.TypeDescription;
+
+import java.util.List;
+
+/**
+ * The DeviceTypeAssembler class is responsible for converting DeviceType domain entities to DeviceTypeDTO data transfer objects.
+ */
+public class DeviceTypeAssembler implements Assembler<DeviceType, DeviceTypeDTO> {
+
+    /**
+     * Constructor of the DeviceTypeAssembler class.
+     */
+    public DeviceTypeAssembler() {
+    }
+
+    /**
+     * Converts a DeviceType domain entity to a DeviceTypeDTO data transfer object.
+     *
+     * @return The DeviceTypeDTO data transfer object.
+     * @throws IllegalArgumentException if deviceType is null.
+     */
+    @Override
+    public DeviceTypeDTO domainToDTO(DeviceType domainEntity) {
+        if (domainEntity == null) {
+            throw new IllegalArgumentException("The DeviceType cannot be null.");
+        }
+
+        String deviceTypeID = domainEntity.getID().toString();
+        String deviceTypeDescription = domainEntity.getDescription().toString();
+
+        DeviceTypeDTO deviceTypeDTO = new DeviceTypeDTO(deviceTypeID, deviceTypeDescription);
+        return deviceTypeDTO;
+    }
+
+    /**
+     * Converts a list of DeviceType domain entities to a list of DeviceTypeDTO data transfer objects.
+     *
+     * @param deviceTypes the list of domain entities to be converted.
+     * @return The list of DeviceTypeDTO data transfer objects.
+     * @throws IllegalArgumentException if deviceTypes is null, empty, or contains null elements.
+     */
+    @Override
+    public List<DeviceTypeDTO> domainToDTO(List<DeviceType> deviceTypes) {
+        if (deviceTypes == null || deviceTypes.isEmpty() || deviceTypes.contains(null)) {
+            throw new IllegalArgumentException("The list of DeviceTypes cannot be null, empty, or contain null elements.");
+        }
+        // Convert each DeviceType entity to DeviceTypeDTO using domainToDTO method
+        List<DeviceTypeDTO> deviceTypesDTOS = deviceTypes.stream().map(this::domainToDTO).toList();
+        return deviceTypesDTOS;
+    }
+
+    /**
+     * Converts a DeviceTypeID object to a string representation.
+     *
+     * @param deviceTypeID the DeviceTypeID object to be converted.
+     * @return The string representation of the DeviceTypeID.
+     */
+    public String deviceTypeIDToString(DeviceTypeID deviceTypeID) {
+        return deviceTypeID.toString();
+    }
+
+    public String deviceTypeDescriptionToString(TypeDescription deviceTypeDescription) {
+        return deviceTypeDescription.toString();
+    }
+}
---
+++ src/test/java/SmartHomeDDD/assembler/DeviceTypeAssemblerTest.java
@@ -0,0 +1,159 @@
+package SmartHomeDDD.assembler;
+
+import SmartHomeDDD.DTO.DeviceTypeDTO;
+import SmartHomeDDD.domain.DeviceType.DeviceType;
+import SmartHomeDDD.valueObject.DeviceTypeID;
+import SmartHomeDDD.valueObject.TypeDescription;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test class for DeviceTypeAssembler.
+ */
+class DeviceTypeAssemblerTest {
+
+
+    @Test
+    public void shouldInstantiateDeviceTypeAssembler() {
+        new DeviceTypeAssembler();
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeIsValid() {
+        //Arrange
+        String deviceTypeID = "Switch Device";
+        String deviceTypeDescription = "Switch Device Description";
+
+        DeviceType deviceType = mock(DeviceType.class);
+        when(deviceType.getID()).thenReturn(mock(DeviceTypeID.class));
+        when(deviceType.getID().toString()).thenReturn(deviceTypeID);
+        when(deviceType.getDescription()).thenReturn(mock(TypeDescription.class));
+        when(deviceType.getDescription().toString()).thenReturn(deviceTypeDescription);
+
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        DeviceTypeDTO expectedDeviceType = new DeviceTypeDTO(deviceTypeID, deviceTypeDescription);
+        //Act
+        DeviceTypeDTO deviceTypeDTO = deviceTypeAssembler.domainToDTO(deviceType);
+        //Assert
+        assertEquals(expectedDeviceType.deviceTypeID, deviceTypeDTO.deviceTypeID);
+
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeIsNull() {
+        // Arrange
+        String DeviceTypeID = "Switch Device";
+        DeviceType deviceType = mock(DeviceType.class); // Criação do mock do objeto DeviceType
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            deviceTypeAssembler.domainToDTO((DeviceType) null);
+        });
+        // Assert
+        assertEquals("The DeviceType cannot be null.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeListIsNull() {
+        // Arrange
+        List<DeviceType> deviceTypes = null;
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            deviceTypeAssembler.domainToDTO(deviceTypes);
+        });
+        // Assert
+        assertEquals("The list of DeviceTypes cannot be null, empty, or contain null elements.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeListIsEmpty() {
+        // Arrange
+        List<DeviceType> deviceTypes = List.of();
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            deviceTypeAssembler.domainToDTO(deviceTypes);
+        });
+        // Assert
+        assertEquals("The list of DeviceTypes cannot be null, empty, or contain null elements.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeListIsValid() {
+        // Arrange
+        String deviceTypeID = "Switch Device";
+        String deviceTypeDescription = "Switch Device Description";
+
+        DeviceType deviceType = mock(DeviceType.class);
+        when(deviceType.getID()).thenReturn(mock(DeviceTypeID.class));
+        when(deviceType.getID().toString()).thenReturn(deviceTypeID);
+        when(deviceType.getDescription()).thenReturn(mock(TypeDescription.class));
+        when(deviceType.getDescription().toString()).thenReturn(deviceTypeDescription);
+
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        List<DeviceType> deviceTypes = Arrays.asList(deviceType, deviceType);
+        DeviceTypeDTO expectedDeviceType = new DeviceTypeDTO(deviceTypeID, deviceTypeDescription);
+        // Act
+        List<DeviceTypeDTO> deviceTypeDTOS = deviceTypeAssembler.domainToDTO(deviceTypes);
+        // Assert
+        assertEquals(2, deviceTypeDTOS.size());
+        assertEquals(expectedDeviceType.deviceTypeID, deviceTypeDTOS.get(0).deviceTypeID);
+        assertEquals(expectedDeviceType.deviceTypeID, deviceTypeDTOS.get(1).deviceTypeID);
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeIDIsValid() {
+        // Arrange
+        DeviceTypeID deviceTypeID = mock(DeviceTypeID.class);
+        when(deviceTypeID.toString()).thenReturn("Switch Device");
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        // Act
+        String deviceTypeIDString = deviceTypeAssembler.deviceTypeIDToString(deviceTypeID);
+        // Assert
+        assertEquals("Switch Device", deviceTypeIDString);
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeDescriptionIsValid() {
+        // Arrange
+        TypeDescription deviceTypeDescription = mock(TypeDescription.class);
+        when(deviceTypeDescription.toString()).thenReturn("Switch Device Description");
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+        // Act
+        String deviceTypeDescriptionString = deviceTypeAssembler.deviceTypeDescriptionToString(deviceTypeDescription);
+        // Assert
+        assertEquals("Switch Device Description", deviceTypeDescriptionString);
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeDescriptionIsNull() {
+        // Arrange
+        TypeDescription deviceTypeDescription = null;
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+
+        // Act & Assert
+        assertThrows(NullPointerException.class, () -> {
+            deviceTypeAssembler.deviceTypeDescriptionToString(deviceTypeDescription);
+        });
+    }
+
+    @Test
+    public void shouldReturnDeviceTypeAssemblerDTO_WhenDeviceTypeIDIsNull() {
+        // Arrange
+        DeviceTypeID deviceTypeID = null;
+        DeviceTypeAssembler deviceTypeAssembler = new DeviceTypeAssembler();
+
+        // Act & Assert
+        assertThrows(NullPointerException.class, () -> {
+            deviceTypeAssembler.deviceTypeIDToString(deviceTypeID);
+        });
+    }
+
+}
