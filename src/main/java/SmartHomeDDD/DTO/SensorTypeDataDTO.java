7f0c537bf7fe0164efb951d774df8a2bd527f4f5
rueloParente <1231858@isep.ipp.pt>
2024-04-03 22:59:03 +0100

    #286 Added SensorTypeDataDTO

---
+++ src/main/java/SmartHomeDDD/DTO/SensorTypeDataDTO.java
@@ -0,0 +1,10 @@
+package SmartHomeDDD.DTO;
+
+public class SensorTypeDataDTO {
+    public String sensorTypeDescription;
+    public String unitID;
+    public SensorTypeDataDTO(String sensorTypeDescription, String unitID) {
+        this.sensorTypeDescription = sensorTypeDescription;
+        this.unitID = unitID;
+    }
+}
