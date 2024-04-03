9dd5ff54f16974d5b6cce74c94b819ee7334338c
manuelcruz98 <1231841@isep.ipp.pt>
2024-04-03 16:38:05 +0100

    #271 refactor: ImpActuatorFactory methods refactor and respective tests

--- src/test/java/SmartHomeDDD/domain/Actuator/ImpActuatorFactoryTest.java
+++ src/test/java/SmartHomeDDD/domain/Actuator/ImpActuatorFactoryTest.java
@@ -1,47 +1,138 @@
 package SmartHomeDDD.domain.Actuator;
 
+import SmartHomeDDD.domain.Sensor.ImpSensorFactory;
+import SmartHomeDDD.domain.Sensor.Sensor;
 import SmartHomeDDD.valueObject.*;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
 
-public class ImpActuatorFactoryTest {
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class ImpActuatorFactoryTest {
 
     /**
      * Test of createActuator method, of class ImpActuatorFactory when the constructor has 4 parameters
-     *
      */
 
     @Test
-    public void shouldCreateAValidActuator_whenTheConstructorHas4Parameters() {
+    void shouldCreateAValidActuator_whenTheConstructorHas4Parameters() {
         //Arrange
-        DeviceID deviceID = new DeviceID("1");
-        ModelPath modelPath = new ModelPath("SmartHomeDDD.domain.Actuator.SwitchActuator");
-        ActuatorTypeID actuatorTypeID = new ActuatorTypeID("1");
-        ActuatorName actuatorName = new ActuatorName("SwitchActuator");
+        DeviceID deviceIDMock = mock(DeviceID.class);
+
+        ModelPath modelPathMock = mock(ModelPath.class);
+        when(modelPathMock.toString()).thenReturn("SmartHomeDDD.domain.Actuator.SwitchActuator");
+
+        ActuatorTypeID actuatorTypeIDMock = mock(ActuatorTypeID.class);
+        ActuatorName actuatorNameMock = mock(ActuatorName.class);
 
         ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
 
         //Act
-        impActuatorFactory.createActuator(deviceID, modelPath, actuatorTypeID, actuatorName);
+        Actuator result = impActuatorFactory.createActuator(deviceIDMock, modelPathMock, actuatorTypeIDMock, actuatorNameMock);
+
+        //Assert
+        assertNotNull(result);
     }
 
+
     /**
-     *
      * Test of createActuator method, of class ImpActuatorFactory when the constructor has more than 4 parameters
      */
 
     @Test
-   public void shouldCreateAValidActuator_whenTheConstructorHasMoreThan4Parameters() {
-       //Arrange
-       DeviceID deviceID = new DeviceID("1");
-       ModelPath modelPath = new ModelPath("SmartHomeDDD.domain.Actuator.SetIntegerActuator");
-       ActuatorTypeID actuatorTypeID = new ActuatorTypeID("1");
-       ActuatorName actuatorName = new ActuatorName("SetIntegerActuator");
-       SetIntegerActuatorLimits limits = new SetIntegerActuatorLimits(1, 10);
+    void shouldCreateAValidActuator_whenTheConstructorHasMoreThan4Parameters() {
+        //Arrange
+        DeviceID deviceIDMock = mock(DeviceID.class);
 
-       ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
+        ModelPath modelPathMock = mock(ModelPath.class);
+        when(modelPathMock.toString()).thenReturn("SmartHomeDDD.domain.Actuator.SetIntegerActuator");
 
-       //Act
-       impActuatorFactory.createActuator(deviceID, modelPath, actuatorTypeID, actuatorName, limits);
-   }
+        ActuatorTypeID actuatorTypeIDMock = mock(ActuatorTypeID.class);
+        ActuatorName actuatorNameMock = mock(ActuatorName.class);
+
+        SetIntegerActuatorLimits limitsMock = mock(SetIntegerActuatorLimits.class);
+
+        ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
+
+        //Act
+        Actuator result = impActuatorFactory.createActuator(deviceIDMock, modelPathMock, actuatorTypeIDMock, actuatorNameMock, limitsMock);
+
+        //Assert
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of createActuator method, of class ImpActuatorFactory when the constructor has less than 4 parameters
+     */
+    @Test
+    void shouldBeNull_whenTheConstructorHasLessThan4Parameters() {
+        //Arrange
+        DeviceID deviceIDMock = mock(DeviceID.class);
+
+        ModelPath modelPathMock = mock(ModelPath.class);
+        when(modelPathMock.toString()).thenReturn("SmartHomeDDD.domain.Actuator.SetIntegerActuator");
+
+        ActuatorTypeID actuatorTypeIDMock = mock(ActuatorTypeID.class);
+
+        ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
+
+        //Act + Assert
+        assertThrows(IllegalArgumentException.class, () -> impActuatorFactory.createActuator(deviceIDMock, modelPathMock, actuatorTypeIDMock));
+    }
+
+
+    /**
+     * Test of createActuator method, of class ImpActuator when the constructor has 4 parameters and the class does not exist
+     */
+    @Test
+    void shouldBeNull_whenNoMatchingClass() {
+        //Arrange
+        DeviceID deviceIDMock = mock(DeviceID.class);
+        ModelPath modelPathMock = mock(ModelPath.class);
+        when(modelPathMock.toString()).thenReturn("NonExistentClass");
+
+        ActuatorTypeID actuatorTypeIDMock = mock(ActuatorTypeID.class);
+
+        ActuatorName actuatorNameMock = mock(ActuatorName.class);
+
+        ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
+
+        //Act
+        Actuator result = impActuatorFactory.createActuator(deviceIDMock, modelPathMock, actuatorTypeIDMock, actuatorNameMock);
+
+        //Assert
+        assertNull(result);
+    }
+
+    /**
+     * Test of createActuator method, of class ImpActuatorFactory when the constructor has 4 parameters and the wrong object type is passed
+     */
+    @Test
+    void shouldBeNull_whenWrongObjectTypeInConstructorParameters() {
+        // Arrange
+        DeviceID deviceIdMock = mock(DeviceID.class);
+        ModelPath modelPathMock = mock(ModelPath.class);
+        when(modelPathMock.toString()).thenReturn("SmartHomeDDD.domain.Actuator.SetIntegerActuator");
+
+        ActuatorTypeID actuatorTypeIdMock = mock(ActuatorTypeID.class);
+        ActuatorName actuatorNameMock = mock(ActuatorName.class);
+        String wrongObject = "wrong object";
+
+        ImpActuatorFactory impActuatorFactory = new ImpActuatorFactory();
+
+        // Act + Assert
+        Actuator result = impActuatorFactory.createActuator(deviceIdMock, modelPathMock, actuatorTypeIdMock, actuatorNameMock, wrongObject);
+
+        // Assert
+        assertNull(result);
+    }
+
 }
+
+
+
