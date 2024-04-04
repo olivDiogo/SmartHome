ad7858e12405c806b6edcc98fa6b06368fd2c6ce
João Mouta <1231833@isep.ipp.pt>
2024-04-04 11:18:52 +0100

    fix: updated InstantPowerConsumptionSensor and InstantPowerConsumptionSensorValue + tests #270

--- src/test/java/SmartHomeDDD/domain/Sensor/InstantPowerConsumptionValueTest.java
+++ src/test/java/SmartHomeDDD/domain/Sensor/InstantPowerConsumptionValueTest.java
@@ -1,5 +1,6 @@
 package SmartHomeDDD.domain.Sensor;
 
+import SmartHomeDDD.domain.Sensor.InstantPowerConsumptionSensor.InstantPowerConsumptionValue;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
