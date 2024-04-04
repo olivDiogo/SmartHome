405e200eab6df6729c3e70274d78d99f9fb65248
ISEP_Switch_QA_Luis <126819903+SwitchQA@users.noreply.github.com>
2024-04-04 11:54:00 +0100

    feat: adds Actuator Configuration Service to populate actuator repository
    #295

---
+++ src/main/java/SmartHomeDDD/service/ActuatorConfigurationService.java
@@ -0,0 +1,128 @@
+package SmartHomeDDD.service;
+
+import SmartHomeDDD.domain.ActuatorModel.ActuatorModel;
+import SmartHomeDDD.domain.ActuatorModel.ActuatorModelFactory;
+import SmartHomeDDD.domain.Unit.Unit;
+import SmartHomeDDD.domain.Unit.UnitFactory;
+import SmartHomeDDD.repository.ActuatorModelRepository;
+import SmartHomeDDD.repository.UnitRepository;
+import SmartHomeDDD.valueObject.*;
+import java.io.File;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+public class ActuatorConfigurationService {
+
+  private ActuatorModelRepository _ActuatorModelRepository;
+  private UnitRepository _unitRepository;
+  private ActuatorModelFactory _ActuatorModelFactory;
+  private UnitFactory _unitFactory;
+
+  public ActuatorConfigurationService(
+      ActuatorModelRepository ActuatorModelRepository,
+      UnitRepository unitRepository,
+      ActuatorModelFactory ActuatorModelFactory,
+      UnitFactory unitFactory)
+      throws InstantiationException {
+    validateActuatorModelRepository(ActuatorModelRepository);
+    validateMeasurementTypeRepository(unitRepository);
+    validateActuatorModelFactory(ActuatorModelFactory);
+    validateUnitFactory(unitFactory);
+    loadDefaultActuatorModels();
+    loadDefaultMeasurementTypes();
+  }
+
+  private void validateActuatorModelRepository(ActuatorModelRepository ActuatorModelRepository) {
+    if (ActuatorModelRepository == null) {
+      throw new IllegalArgumentException("Please enter a valid Actuator model repository.");
+    } else {
+      this._ActuatorModelRepository = ActuatorModelRepository;
+    }
+  }
+
+  private void validateMeasurementTypeRepository(UnitRepository unitRepository) {
+    if (unitRepository == null) {
+      throw new IllegalArgumentException("Please enter a valid measurement type repository.");
+    } else {
+      this._unitRepository = unitRepository;
+    }
+  }
+
+  private void validateActuatorModelFactory(ActuatorModelFactory ActuatorModelFactory) {
+    if (ActuatorModelFactory == null) {
+      throw new IllegalArgumentException("Please enter a valid Actuator model factory.");
+    } else {
+      this._ActuatorModelFactory = ActuatorModelFactory;
+    }
+  }
+
+  private void validateUnitFactory(UnitFactory unitFactory) {
+    if (unitFactory == null) {
+      throw new IllegalArgumentException("Please enter a valid unit factory.");
+    } else {
+      this._unitFactory = unitFactory;
+    }
+  }
+
+  private void loadDefaultActuatorModels() throws InstantiationException {
+    Configurations configs = new Configurations();
+    try {
+      Configuration config =
+          configs.properties(
+              new File("configDDD.properties")); // e.g. filePathname = "config.properties"
+
+      // access configuration properties
+      String[] arrayStringClassesActuators = config.getStringArray("Actuator");
+      for (String Actuator : arrayStringClassesActuators) {
+        String ActuatorPathStr =
+            Actuator.split(";")[0]; // String containing the path of the Actuator
+        String ActuatorModelName =
+            ActuatorPathStr.substring(
+                ActuatorPathStr.lastIndexOf('.') + 1); // String containing the Actuator model name
+        String ActuatorTypeIDstr = Actuator.split(";")[1]; // String containing the Actuator type ID
+
+        ModelPath ActuatorPath = new ModelPath(ActuatorPathStr);
+        ActuatorModelName ActuatorName = new ActuatorModelName(ActuatorModelName);
+        ActuatorTypeID ActuatorTypeID = new ActuatorTypeID(ActuatorTypeIDstr);
+
+        ActuatorModel ActuatorModel =
+            _ActuatorModelFactory.createActuatorModel(ActuatorName, ActuatorPath, ActuatorTypeID);
+        _ActuatorModelRepository.save(ActuatorModel);
+      }
+    } catch (ConfigurationException exception) {
+      // Something went wrong
+      throw new InstantiationException(
+          "something went wrong in reading the configuration: " + exception.getMessage());
+    }
+  }
+
+  private void loadDefaultMeasurementTypes() throws InstantiationException {
+    Configurations configs = new Configurations();
+    try {
+      Configuration config =
+          configs.properties(
+              new File("configDDD.properties")); // e.g. filePathname = "config.properties"
+
+      // access configuration properties
+      String[] arrayStringMeasurementTypes = config.getStringArray("measurement");
+      for (String measurement : arrayStringMeasurementTypes) {
+        String[] measurementTypes = measurement.split(";");
+
+        String measurementTypeDescription = measurementTypes[0].split(":")[1];
+        UnitDescription measurementDescription = new UnitDescription(measurementTypeDescription);
+
+        String measurementTypeUnit = measurementTypes[1].split(":")[1];
+        UnitSymbol measurementTUnit = new UnitSymbol(measurementTypeUnit);
+
+        Unit measurementType =
+            _unitFactory.createMeasurement(measurementDescription, measurementTUnit);
+        _unitRepository.save(measurementType);
+      }
+    } catch (ConfigurationException exception) {
+      // Something went wrong
+      throw new InstantiationException(
+          "something went wrong in reading the configuration: " + exception.getMessage());
+    }
+  }
+}
