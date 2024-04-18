debdfd60ed0fa454da3356f35eaed208733632b1
Lloyd <1231838@isep.ipp.pt>
2024-04-18 13:44:28 +0100

    fix: implement validation test for DewPointSensorAggregateTest
    address #373

--- src/test/java/smart_home/domain/sensor/dew_point_sensor/DewPointSensorAggregateTest.java
+++ src/test/java/smart_home/domain/sensor/dew_point_sensor/DewPointSensorAggregateTest.java
@@ -30,30 +30,7 @@ class DewPointSensorAggregateTest {
 
     }
 
-    /**
-     * Test to check if the DewPointSensor is instantiated correctly with a SensorID.
-     */
-    @Test
-    void shouldInstantiateDewPointSensor_WhenParametersAreValidWithSensorID() {
-        String deviceIDName = "123A";
-        String modelPathName = "SmartHome.sensors.DewPointSensor";
-        String name = "DewPointSensor";
-        String typeID = "DewPoint";
-        String sensorIDName = "1234";
 
-        DeviceID deviceID = new DeviceID(deviceIDName);
-        ModelPath modelPath = new ModelPath(modelPathName);
-        SensorName sensorName = new SensorName(name);
-        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
-        SensorID sensorID = new SensorID(sensorIDName);
-
-        // act
-        DewPointSensor dewPointSensor = new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID);
-
-        //assert
-        assertNotNull(dewPointSensor);
-
-    }
 
     /**
      * Test to check if the DewPointSensor throws an exception when the DeviceID is null.
@@ -508,5 +485,166 @@ class DewPointSensorAggregateTest {
 
         //Assert
         assertEquals(expected, result);
+    }
+    /**
+     * Test to check if the DewPointSensor is instantiated correctly with a SensorID.
+     */
+    @Test
+    void shouldInstantiateDewPointSensor_WhenParametersAreValidWithSensorID() {
+        String deviceIDName = "123A";
+        String modelPathName = "SmartHome.sensors.DewPointSensor";
+        String name = "DewPointSensor";
+        String typeID = "DewPoint";
+        String sensorIDName = "1234";
+
+        DeviceID deviceID = new DeviceID(deviceIDName);
+        ModelPath modelPath = new ModelPath(modelPathName);
+        SensorName sensorName = new SensorName(name);
+        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
+        SensorID sensorID = new SensorID(sensorIDName);
+
+        // act
+        DewPointSensor dewPointSensor = new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID);
+
+        //assert
+        assertNotNull(dewPointSensor);
+
+    }
+    /**
+     * Test to check if the DewPointSensor throws an exception when the SensorID is null
+     */
+    @Test
+    void shouldThrowException_WhenSensorIDIsNull() {
+        //Arrange
+        String deviceIDName = "123A";
+        String modelPathName = "SmartHome.sensors.DewPointSensor";
+        String name = "DewPointSensor";
+        String typeID = "DewPoint";
+
+        DeviceID deviceID = new DeviceID(deviceIDName);
+        ModelPath modelPath = new ModelPath(modelPathName);
+        SensorName sensorName = new SensorName(name);
+        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
+        SensorID sensorID = null;
+
+        String expectedMessage = "SensorID is required";
+
+        //Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID)
+        );
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+
+    }
+
+    /**
+     * Test DewPointSensor whit valid SensorID but null DeviceID.
+     */
+    @Test
+    void shouldThrowException_WhenDeviceIDIsNullWithSensorID() {
+        //Arrange
+        String modelPathName = "SmartHome.sensors.DewPointSensor";
+        String name = "DewPointSensor";
+        String typeID = "DewPoint";
+        String sensorIDName = "1234";
+
+        DeviceID deviceID = null;
+        ModelPath modelPath = new ModelPath(modelPathName);
+        SensorName sensorName = new SensorName(name);
+        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
+        SensorID sensorID = new SensorID(sensorIDName);
+
+        String expectedMessage = "DeviceID is required";
+
+        //Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID)
+        );
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+    /**
+     * Test DewPointSensor whit valid SensorID but null ModelPath.
+     */
+    @Test
+    void shouldThrowException_WhenModelPathIsNullWithSensorID() {
+        //Arrange
+        String deviceIDName = "123A";
+        String name = "DewPointSensor";
+        String typeID = "DewPoint";
+        String sensorIDName = "1234";
+
+        DeviceID deviceID = new DeviceID(deviceIDName);
+        ModelPath modelPath = null;
+        SensorName sensorName = new SensorName(name);
+        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
+        SensorID sensorID = new SensorID(sensorIDName);
+
+        String expectedMessage = "ModelPath is required";
+
+        //Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID)
+        );
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+    /**
+     * Test DewPointSensor whit valid SensorID but null SensorName.
+     */
+    @Test
+    void shouldThrowException_WhenSensorNameIsNullWithSensorID() {
+        //Arrange
+        String deviceIDName = "123A";
+        String modelPathName = "SmartHome.sensors.DewPointSensor";
+        String typeID = "DewPoint";
+        String sensorIDName = "1234";
+
+        DeviceID deviceID = new DeviceID(deviceIDName);
+        ModelPath modelPath = new ModelPath(modelPathName);
+        SensorName sensorName = null;
+        SensorTypeID sensorTypeID = new SensorTypeID(typeID);
+        SensorID sensorID = new SensorID(sensorIDName);
+
+        String expectedMessage = "SensorName is required";
+
+        //Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID)
+        );
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+    /**
+     * Test DewPointSensor whit valid SensorID but null SensorTypeID.
+     */
+    @Test
+    void shouldThrowException_WhenSensorTypeIDIsNullWithSensorID() {
+        //Arrange
+        String deviceIDName = "123A";
+        String modelPathName = "SmartHome.sensors.DewPointSensor";
+        String name = "DewPointSensor";
+        String sensorIDName = "1234";
+
+        DeviceID deviceID = new DeviceID(deviceIDName);
+        ModelPath modelPath = new ModelPath(modelPathName);
+        SensorName sensorName = new SensorName(name);
+        SensorTypeID sensorTypeID = null;
+        SensorID sensorID = new SensorID(sensorIDName);
+
+        String expectedMessage = "SensorTypeID is required";
+
+        //Act + Assert
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new DewPointSensor(deviceID, modelPath, sensorTypeID, sensorName, sensorID)
+        );
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
     }
 }
