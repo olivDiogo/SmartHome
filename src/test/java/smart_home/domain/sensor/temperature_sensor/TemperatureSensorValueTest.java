19ebfaed412aec1676c3b1ac0ba4ae153fcab61b
simao-campos87 <1231859@isep.ipp.pt>
2024-04-23 11:53:41 +0100

    #399 added TemperatureSensorValueTest for equals and hascode methods, and mutation killed

--- src/test/java/smart_home/domain/sensor/temperature_sensor/TemperatureSensorValueTest.java
+++ src/test/java/smart_home/domain/sensor/temperature_sensor/TemperatureSensorValueTest.java
@@ -30,7 +30,7 @@ class TemperatureSensorValueTest {
     void shouldThrowExceptionWhenTemoreatureIsLowerThanMinimum(){
 
         //Arrange
-        double temperature = -300.0;
+        double temperature = -273.16;
 
         String expectedMessage = "Temperature value must be above or equal to -273.15";
 
@@ -48,11 +48,12 @@ class TemperatureSensorValueTest {
     void shouldReturnStringValueOfTemperature(){
 
         //Arrange
-        double temperature = 10.5;
+        double temperature =
+                -273.15;
 
         TemperatureSensorValue temperatureSensorValue = new TemperatureSensorValue(temperature);
 
-        String expectedValue = "10.5";
+        String expectedValue = "-273.15";
 
         //Act
         String result = new TemperatureSensorValue(temperature).toString();
@@ -136,5 +137,41 @@ class TemperatureSensorValueTest {
         assertEquals(temperatureSensorValue.hashCode(), result);
     }
 
+    /**
+     * Test if the hashCode method returns the value for TemperatureSensorValue.
+     */
+    @Test
+    void shouldReturnEqualHashCodeForIdenticalObjects(){
 
+        //Arrange
+        double temperature = 10;
+
+        TemperatureSensorValue temperatureSensorValue = new TemperatureSensorValue(temperature);
+        TemperatureSensorValue temperatureSensorValue2 = new TemperatureSensorValue(temperature);
+        boolean expected = temperatureSensorValue.equals(temperatureSensorValue2);
+        //Act
+        boolean result = temperatureSensorValue.hashCode() == temperatureSensorValue2.hashCode();
+
+        //Assert
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Test if hashCode returns diferent from zero
+     */
+    @Test
+    void shouldReturnNotEqualsForHashCodeDifferentFromZero(){
+        //Arrange
+        double temperature = 0;
+        double temperature1 = 10;
+
+        TemperatureSensorValue temperatureSensorValue = new TemperatureSensorValue(temperature);
+        TemperatureSensorValue temperatureSensorValue2 = new TemperatureSensorValue(temperature1);
+        int expected = temperatureSensorValue.hashCode();
+        //Act
+        int result = temperatureSensorValue2.hashCode();
+
+        //Assert
+        assertNotEquals(expected,result);
+    }
 }
