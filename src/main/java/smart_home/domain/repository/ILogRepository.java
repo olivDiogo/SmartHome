c5601095c7ca8aaa8ea5291bc4d7c7c3db9aca51
manuelcruz98 <1231841@isep.ipp.pt>
2024-04-23 14:12:46 +0100

    new: interface ILogRepository and LogRepository class#397

---
+++ src/main/java/smart_home/domain/repository/ILogRepository.java
@@ -0,0 +1,24 @@
+package smart_home.domain.repository;
+
+import smart_home.ddd.IRepository;
+import smart_home.domain.Log;
+import smart_home.value_object.DatePeriod;
+import smart_home.value_object.DeviceID;
+import smart_home.value_object.LogID;
+
+import java.util.List;
+
+
+public interface ILogRepository extends IRepository<LogID, Log> {
+
+
+    /**
+     * Method to find logs by device and time period
+     *
+     * @param deviceID DeviceID object
+     * @param period DatePeriod object
+     * @return List of Log
+     */
+    List<Log> findByDeviceIDAndTimePeriod (DeviceID deviceID, DatePeriod period);
+
+}
---
+++ src/main/java/smart_home/persistence/mem/LogRepository.java
@@ -0,0 +1,99 @@
+package smart_home.persistence.mem;
+
+import smart_home.domain.repository.ILogRepository;
+import smart_home.utils.Validator;
+import smart_home.value_object.DatePeriod;
+import smart_home.value_object.DeviceID;
+
+import smart_home.value_object.LogID;
+
+import java.util.*;
+
+import smart_home.domain.Log;
+
+
+public class LogRepository implements ILogRepository {
+
+    private final Map<LogID, Log> logData = new LinkedHashMap<>();
+
+
+    /**
+     * Method to save log
+     *
+     * @param log Log object
+     * @return Log object
+     */
+    @Override
+    public Log save(Log log) {
+        Validator.validateNotNull(log, "Log");
+
+        if (containsOfIdentity(log.getID())) {
+            throw new IllegalArgumentException("Log already exists.");
+        } else {
+            logData.put(log.getID(), log);
+        }
+        return log;
+    }
+
+
+    /**
+     * Method to find all logs
+     *
+     * @return List of Log
+     */
+    @Override
+    public List<Log> findAll() {
+        return List.copyOf(logData.values().stream().toList());
+    }
+
+
+    /**
+     * Method to find log by ID
+     *
+     * @param logID LogID object
+     * @return Optional of Log
+     */
+    @Override
+    public Optional<Log> ofIdentity(LogID logID) {
+        return Optional.ofNullable(logData.get(logID));
+    }
+
+
+    /**
+     * Method to check if log exists
+     *
+     * @param logID LogID object
+     * @return boolean
+     */
+    @Override
+    public boolean containsOfIdentity(LogID logID) {
+        return logData.containsKey(logID);
+    }
+
+
+    /**
+     * Method to find logs by device ID and time period
+     *
+     * @param deviceID DeviceID object
+     * @param period   DatePeriod object
+     * @return List of Log
+     */
+    @Override
+    public List<Log> findByDeviceIDAndTimePeriod(DeviceID deviceID, DatePeriod period) {
+
+        List<Log> logList = new ArrayList<>(logData.values());
+
+        List<Log> logListByDeviceID = new ArrayList<>();
+
+        for (Log log : logList) {
+            if (log.getDeviceID().getID().equals(deviceID.getID()) && log.getTimeStamp().isAfter(period.getStartDate().minusMinutes(1))
+                    && log.getTimeStamp().isBefore(period.getEndDate().plusMinutes(1))) {
+                logListByDeviceID.add(log);
+            }
+        }
+
+        return logListByDeviceID;
+    }
+
+
+}
