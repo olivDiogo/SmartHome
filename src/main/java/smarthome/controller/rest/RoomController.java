cadeafd0e4564b2f60b87e4681286c06d5d93335
ISEP_Switch_QA_Luis <1222637@isep.ipp.pt>
2024-05-06 18:38:39 +0100

    feat: add get all rooms and get room by id to Room Rest Controller + tests
    #447

--- src/main/java/smarthome/controller/rest/RoomController.java
+++ src/main/java/smarthome/controller/rest/RoomController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,12 @@ import smarthome.domain.service.IRoomService;
 import smarthome.domain.value_object.Dimension;
 import smarthome.domain.value_object.HouseID;
 import smarthome.domain.value_object.RoomFloor;
+import smarthome.domain.value_object.RoomID;
 import smarthome.domain.value_object.RoomName;
 import smarthome.utils.dto.RoomDTO;
 import smarthome.utils.dto.RoomDataDTO;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/room")
@@ -35,6 +40,12 @@ public class RoomController {
     this.roomService = roomService;
   }
 
+  /**
+   * Adds a new room to the house with the provided house ID.
+   *
+   * @param data
+   * @return The room that was added.
+   */
   @PostMapping("/add")
   public ResponseEntity<RoomDTO> addRoom(@Valid @RequestBody RoomDataDTO data){
     HouseID houseID = new HouseID(data.houseID);
@@ -44,5 +55,36 @@ public class RoomController {
     Room room = roomService.addRoom(houseID, name, dimension, floor);
     RoomDTO roomDTO = roomAssembler.domainToDTO(room);
     return ResponseEntity.status(HttpStatus.CREATED).body(roomDTO);
+  }
+
+  /**
+   * Get all rooms
+   *
+   * @return a list of all rooms
+   */
+  @GetMapping("/all")
+  public ResponseEntity<List<RoomDTO>> getAllRooms() {
+    List<Room> rooms = roomService.getAllRooms();
+    List<RoomDTO> roomDTOs = roomAssembler.domainToDTO(rooms);
+    return ResponseEntity.status(HttpStatus.CREATED).body(roomDTOs);
+  }
+
+  /**
+   * Get a room by ID
+   *
+   * @param idStr
+   * @return the room with the given ID
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<RoomDTO> getRoomById(@PathVariable("id") String idStr) {
+    RoomID id = new RoomID(idStr);
+    Optional<Room> room = roomService.getRoomById(id);
+    if (room.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+    else {
+      RoomDTO roomDTO = roomAssembler.domainToDTO(room.get());
+      return ResponseEntity.status(HttpStatus.CREATED).body(roomDTO);
+    }
   }
 }
--- src/test/java/smarthome/controller/rest/RoomControllerTest.java
+++ src/test/java/smarthome/controller/rest/RoomControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,11 +15,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import smarthome.domain.house.House;
+import smarthome.domain.room.Room;
+import smarthome.domain.room.RoomFactoryImpl;
 import smarthome.domain.value_object.Address;
+import smarthome.domain.value_object.Dimension;
 import smarthome.domain.value_object.GPS;
+import smarthome.domain.value_object.HouseID;
+import smarthome.domain.value_object.RoomFloor;
+import smarthome.domain.value_object.RoomName;
 import smarthome.domain.value_object.postal_code.PostalCodeFactory;
 import smarthome.persistence.mem.HouseRepository;
+import smarthome.persistence.mem.RoomRepository;
 import smarthome.utils.dto.RoomDataDTO;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -34,6 +43,12 @@ class RoomControllerTest {
   @MockBean
   private HouseRepository houseRepository;
 
+  @MockBean
+  private RoomRepository roomRepository;
+
+  @Autowired
+  private RoomFactoryImpl roomFactory;
+
   House setupHouse() {
     // Arrange
     String street = "Rua de Sao Bento";
@@ -48,6 +63,25 @@ class RoomControllerTest {
     return new House(address, gps);
   }
 
+  RoomDataDTO setupRoomDataDTO(House house) {
+    String houseIDStr = house.getID().toString();
+    String name = "Living Room";
+    int floor = 1;
+    int width = 10;
+    int length = 10;
+    int height = 3;
+    return new RoomDataDTO(houseIDStr, name, floor, width, length, height);
+  }
+
+  // given a RoomDataDTO create a Room object using RoomFactory
+  Room setupRoom(RoomDataDTO roomDataDTO) {
+    HouseID houseID = new HouseID(roomDataDTO.houseID);
+    RoomName name = new RoomName(roomDataDTO.name);
+    RoomFloor floor = new RoomFloor(roomDataDTO.floor);
+    Dimension dimension = new Dimension(roomDataDTO.width, roomDataDTO.length, roomDataDTO.height);
+    return roomFactory.createRoom(houseID, name, dimension, floor);
+  }
+
   /**
    * Verify that a Room is correctly added to the House
    */
@@ -55,13 +89,7 @@ class RoomControllerTest {
   void shouldReturnRoomDTO_whenRoomIsAddedToHouse() throws Exception {
     // Arrange
     House house = setupHouse();
-    String houseIDStr = house.getID().toString();
-    String name = "Living Room";
-    int floor = 1;
-    int width = 10;
-    int length = 10;
-    int height = 3;
-    RoomDataDTO roomDataDTO = new RoomDataDTO(houseIDStr, name, floor, width, length, height);
+    RoomDataDTO roomDataDTO = setupRoomDataDTO(house);
     when(houseRepository.ofIdentity(house.getID())).thenReturn(Optional.of(house));
 
     // Act & Assert
@@ -70,5 +98,91 @@ class RoomControllerTest {
             .content(objectMapper.writeValueAsString(roomDataDTO)))
         .andExpect(status().isCreated())
         .andExpect(jsonPath("$.roomName").value("Living Room"));
+  }
+
+  /**
+   * Verify that a Room is not added to the House when the House does not exist
+   */
+  @Test
+  void shouldReturnBadRequest_whenHouseDoesNotExist() throws Exception {
+    // Arrange
+    String houseIDStr = "1";
+    HouseID houseID = new HouseID(houseIDStr);
+    String name = "Living Room";
+    int floor = 1;
+    int width = 10;
+    int length = 10;
+    int height = 3;
+    RoomDataDTO roomDataDTO = new RoomDataDTO(houseIDStr, name, floor, width, length, height);
+    when(houseRepository.ofIdentity(houseID)).thenReturn(Optional.empty());
+
+    // Act & Assert
+    mockMvc.perform(post("/room/add")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(roomDataDTO)))
+        .andExpect(status().isBadRequest());
+  }
+
+  /**
+   * Test getAllRooms method in RoomController other test method
+   */
+  @Test
+  void shouldReturnAllRooms_whenGetAllRoomsIsCalled() throws Exception {
+    //Arrange
+    House house = setupHouse();
+    when(houseRepository.ofIdentity(house.getID())).thenReturn(Optional.of(house));
+
+    RoomDataDTO dto1 = setupRoomDataDTO(house);
+    RoomDataDTO dto2 = setupRoomDataDTO(house);
+
+    Room room = setupRoom(dto1);
+    Room room2 = setupRoom(dto2);
+
+    when(roomRepository.findAll()).thenReturn(List.of(room, room2));
+
+    //Act & Assert
+    mockMvc.perform(get("/room/all"))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$[0].roomName").value("Living Room"))
+        .andExpect(jsonPath("$[1].roomName").value("Living Room"));
+  }
+
+  //TODO: test getAllRooms with no rooms. Should return an empty list or an error?
+
+  /**
+   * Test getRoomById method in RoomController
+   */
+  @Test
+  void shouldReturnRoom_whenGetRoomById() throws Exception {
+    // Arrange
+    House house = setupHouse();
+    when(houseRepository.ofIdentity(house.getID())).thenReturn(Optional.of(house));
+
+    RoomDataDTO roomDataDTO = setupRoomDataDTO(house);
+    Room room = setupRoom(roomDataDTO);
+    when(roomRepository.ofIdentity(room.getID())).thenReturn(Optional.of(room));
+
+    // Act & Assert
+    mockMvc.perform(get("/room/" + room.getID()))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.roomName").value("Living Room"));
+  }
+
+  /**
+   * Test getRoomById method in RoomController when room does not exist
+   */
+  @Test
+  void shouldReturnNotFound_whenRoomDoesNotExist() throws Exception {
+    // Arrange
+    House house = setupHouse();
+    when(houseRepository.ofIdentity(house.getID())).thenReturn(Optional.of(house));
+
+    RoomDataDTO roomDataDTO = setupRoomDataDTO(house);
+    Room room = setupRoom(roomDataDTO);
+    when(roomRepository.ofIdentity(room.getID())).thenReturn(Optional.empty());
+
+    // Act & Assert
+    mockMvc.perform(get("/room/" + room.getID()))
+        .andExpect(status().isNotFound());
   }
 }
