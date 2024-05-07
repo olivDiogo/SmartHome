74faa39faee52149ccd366ed6191d72ca056abed
manuelcruz98 <1231841@isep.ipp.pt>
2024-05-07 12:32:09 +0100

    added test to HTTP REST SensorTypeController #449

--- src/test/java/smarthome/controller/rest/SensorTypeControllerTest.java
+++ src/test/java/smarthome/controller/rest/SensorTypeControllerTest.java
@@ -59,5 +59,53 @@ class SensorTypeControllerTest {
         .andExpect(jsonPath("$.sensorTypeDescription").value(sensorTypeDescription))
         .andExpect(jsonPath("$.unitID").value(unitID));
   }
+
+  /**
+   * Verify that a SensorType is not created when the description is null
+   */
+  @Test
+  void shouldReturnBadRequest_whenSensorTypeDescriptionIsNull() throws Exception {
+    // Arrange
+    String unitID = "Celsius";
+    SensorTypeDataDTO sensorTypeDataDTO = new SensorTypeDataDTO(null, unitID);
+
+    // Act & Assert
+    mockMvc.perform(post("/sensorType/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(sensorTypeDataDTO)))
+        .andExpect(status().isBadRequest());
+  }
+
+
+  /**
+   * Verify that a SensorType is not created when the unitID is null
+   */
+  @Test
+  void shouldReturnBadRequest_whenUnitIDIsNull() throws Exception {
+    // Arrange
+    String sensorTypeDescription = "Temperature";
+    SensorTypeDataDTO sensorTypeDataDTO = new SensorTypeDataDTO(sensorTypeDescription, null);
+
+    // Act & Assert
+    mockMvc.perform(post("/sensorType/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(sensorTypeDataDTO)))
+        .andExpect(status().isBadRequest());
+  }
+
+  /**
+   * This test case verifies that the ActuatorTypeController returns a 404 Not Found status when no
+   * actuator types are available.
+   */
+  @Test
+  void shouldReturnNotFoundWhenNoSensorTypesAvailable() throws Exception {
+    // Arrange: Configure the service to return an empty list when called
+    when(sensorTypeService.getAllSensorTypes()).thenReturn(null);
+
+    // Act & Assert: Perform the request and expect a 404 Not Found status
+    mockMvc.perform(post("/sensorType/all")
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
+  }
 }
 
