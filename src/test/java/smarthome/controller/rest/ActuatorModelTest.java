a81d3e61e4533bc22e66d26c7896cbb898eb8b86
simao-campos87 <1231859@isep.ipp.pt>
2024-05-07 18:47:50 +0100

    #455 added ActuatorModel rest comtroller and tests

--- src/test/java/smarthome/controller/rest/ActuatorModelTest.java
+++ src/test/java/smarthome/controller/rest/ActuatorModelTest.java
@@ -1,15 +1,10 @@
 package smarthome.controller.rest;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,30 +12,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import smarthome.domain.actuator_model.ActuatorModel;
-import smarthome.domain.actuator_model.ActuatorModelFactoryImpl;
 import smarthome.domain.actuator_model.IActuatorModelFactory;
-import smarthome.domain.actuator_type.ActuatorType;
-import smarthome.domain.repository.IActuatorModelRepository;
-import smarthome.domain.repository.ISensorModelRepository;
-import smarthome.domain.repository.IUnitRepository;
-import smarthome.domain.sensor_model.ISensorModelFactory;
 import smarthome.domain.service.IActuatorModelService;
-import smarthome.domain.unit.IUnitFactory;
-import smarthome.domain.unit.UnitFactoryImpl;
 import smarthome.domain.value_object.ActuatorModelName;
 import smarthome.domain.value_object.ActuatorTypeID;
 import smarthome.domain.value_object.ModelPath;
 import smarthome.mapper.ActuatorModelAssembler;
-import smarthome.persistence.mem.ActuatorModelRepository;
-import smarthome.persistence.mem.SensorModelRepository;
-import smarthome.persistence.mem.UnitRepository;
-import smarthome.utils.LoadModelsAndUnit;
 import smarthome.utils.dto.ActuatorModelDTO;
-import smarthome.utils.dto.ActuatorTypeDTO;
-import smarthome.utils.dto.actuator_data_dto.ActuatorDataGenericDTOImp;
-
 import java.util.List;
-
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
