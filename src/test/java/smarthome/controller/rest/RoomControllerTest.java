551fcc6fff2365876c257a8d5a4fe2e379cd4bc8
BernardoAmorim <1110281@isep.ipp.pt>
2024-05-10 13:37:31 +0100

    feat: add method getDevicesInAGivenRoom to RoomController;
    Implemented a new endpoint in the RoomController to fetch all devices belonging to a specific room. This endpoint allows clients to retrieve
    information about devices within a room by providing the room ID.
    
    addresses #469

--- src/test/java/smarthome/controller/rest/RoomControllerTest.java
+++ src/test/java/smarthome/controller/rest/RoomControllerTest.java
@@ -1,12 +1,14 @@
 package smarthome.controller.rest;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,21 +16,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import smarthome.ddd.IAssembler;
+import smarthome.domain.device.Device;
+import smarthome.domain.device.DeviceFactoryImpl;
+import smarthome.domain.device.IDeviceFactory;
 import smarthome.domain.house.House;
 import smarthome.domain.repository.IHouseRepository;
 import smarthome.domain.repository.IRoomRepository;
+import smarthome.domain.room.IRoomFactory;
 import smarthome.domain.room.Room;
-import smarthome.domain.room.RoomFactoryImpl;
+import smarthome.domain.service.IDeviceService;
 import smarthome.domain.value_object.Address;
+import smarthome.domain.value_object.DeviceName;
+import smarthome.domain.value_object.DeviceStatus;
+import smarthome.domain.value_object.DeviceTypeID;
 import smarthome.domain.value_object.Dimension;
 import smarthome.domain.value_object.GPS;
 import smarthome.domain.value_object.HouseID;
 import smarthome.domain.value_object.RoomFloor;
 import smarthome.domain.value_object.RoomName;
 import smarthome.domain.value_object.postal_code.PostalCodeFactory;
+import smarthome.utils.dto.DeviceDTO;
 import smarthome.utils.dto.data_dto.RoomDataDTO;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,8 +55,17 @@ class RoomControllerTest {
   @MockBean
   private IRoomRepository roomRepository;
 
+  @MockBean
+  private IDeviceService deviceService;
+
+  @MockBean
+  private IAssembler<Device, DeviceDTO> deviceAssembler;
+
   @Autowired
-  private RoomFactoryImpl roomFactory;
+  private IRoomFactory roomFactory;
+
+  @Autowired
+  private IDeviceFactory deviceFactory;
 
   House setupHouse() {
     // Arrange
@@ -184,5 +202,43 @@ class RoomControllerTest {
     // Act & Assert
     mockMvc.perform(get("/room/" + room.getID()))
         .andExpect(status().isNotFound());
+  }
+
+  /**
+   * test getDeviceByRoomId method in RoomController
+   */
+  @Test
+  void shouldReturnDevices_whenGetDevicesByRoomId() throws Exception {
+    // Arrange
+    House house = setupHouse();
+    when(houseRepository.ofIdentity(house.getID())).thenReturn(Optional.of(house));
+
+    RoomDataDTO roomDataDTO = setupRoomDataDTO(house);
+    Room room = setupRoom(roomDataDTO);
+    when(roomRepository.ofIdentity(room.getID())).thenReturn(Optional.of(room));
+
+    IDeviceFactory deviceFactory = new DeviceFactoryImpl();
+    DeviceName deviceName = new DeviceName("device1");
+    DeviceStatus deviceStatus = new DeviceStatus(true);
+    DeviceTypeID deviceTypeID = new DeviceTypeID("device1");
+    Device device1 = deviceFactory.createDevice(room.getID(), deviceName, deviceStatus,
+        deviceTypeID);
+    Device device2 = deviceFactory.createDevice(room.getID(), deviceName, deviceStatus,
+        deviceTypeID); // Replace with actual Device object
+    List<Device> devices = List.of(device1, device2);
+    when(deviceService.getDevicesByRoomId(room.getID())).thenReturn(devices);
+
+    DeviceDTO deviceDTO1 = new DeviceDTO(device1.getID().toString(), room.getID().toString(),
+        deviceName.toString(), deviceStatus.toString());
+    DeviceDTO deviceDTO2 = new DeviceDTO(device2.getID().toString(), room.getID().toString(),
+        deviceName.toString(), deviceStatus.toString());
+    when(deviceAssembler.domainToDTO(device1)).thenReturn(deviceDTO1);
+    when(deviceAssembler.domainToDTO(device2)).thenReturn(deviceDTO2);
+
+    // Act & Assert
+    mockMvc.perform(get("/room/" + room.getID() + "/devices")
+            .contentType(MediaType.APPLICATION_JSON)) // Set content type to JSON
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$._links.self").exists());
   }
 }
