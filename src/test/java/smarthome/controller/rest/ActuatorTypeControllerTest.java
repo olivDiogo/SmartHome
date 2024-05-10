dd679534e4603314b0f5897da210e4a417f55ae9
BernardoAmorim <1110281@isep.ipp.pt>
2024-05-10 20:06:31 +0100

    fix: update ActuatorTypeController tests
    addresses #451

--- src/test/java/smarthome/controller/rest/ActuatorTypeControllerTest.java
+++ src/test/java/smarthome/controller/rest/ActuatorTypeControllerTest.java
@@ -1,82 +1,64 @@
 package smarthome.controller.rest;
 
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import smarthome.ddd.IAssembler;
 import smarthome.domain.actuator_type.ActuatorType;
-import smarthome.domain.actuator_type.IActuatorTypeFactory;
 import smarthome.domain.service.IActuatorTypeService;
-import smarthome.domain.value_object.TypeDescription;
-import smarthome.domain.value_object.UnitID;
-import smarthome.mapper.ActuatorTypeAssembler;
 import smarthome.utils.dto.ActuatorTypeDTO;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ActuatorTypeController.class)
 class ActuatorTypeControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @Autowired
-  private IActuatorTypeFactory actuatorTypeFactory;
-
   @MockBean
   private IActuatorTypeService actuatorTypeService;
 
   @MockBean
-  private ActuatorTypeAssembler actuatorTypeAssembler;
-
+  private IAssembler<ActuatorType, ActuatorTypeDTO> actuatorTypeAssembler;
 
   /**
-   * This test case verifies that the ActuatorTypeController returns a list of actuator types when
-   * they are found.
+   * This test case verifies that the ActuatorTypeController returns a list of actuator types when they are found.
    */
   @Test
   void shouldReturnActuatorTypes_WhenFound() throws Exception {
     // Arrange
-    String actuatorTypeID = "actuatorTypeID";
-    String actuatorTypeDescription = "actuatorTypeDescription";
-    String unit = "unit";
-    String strUnitID = "unitID";
-    ActuatorTypeDTO actuatorTypeDTO = new ActuatorTypeDTO(actuatorTypeID, actuatorTypeDescription,
-        unit);
-    List<ActuatorTypeDTO> actuatorTypeDTOs = List.of(actuatorTypeDTO);
-    TypeDescription typeDescription = new TypeDescription(actuatorTypeDescription);
-    UnitID unitID = new UnitID(strUnitID);
-    ActuatorType actuatorType = actuatorTypeFactory.createActuatorType(typeDescription, unitID);
-    when(actuatorTypeService.getAllActuatorTypes()).thenReturn(List.of(actuatorType));
-    when(actuatorTypeAssembler.domainToDTO((List<ActuatorType>) any())).thenReturn(
-        actuatorTypeDTOs);
+    ActuatorType actuatorType = mock(ActuatorType.class);
+    ActuatorTypeDTO actuatorTypeDTO = mock(ActuatorTypeDTO.class);
+    when(actuatorTypeService.getAllActuatorTypes()).thenReturn(
+        Collections.singletonList(actuatorType));
+    when(actuatorTypeAssembler.domainToDTO(Collections.singletonList(actuatorType)))
+        .thenReturn(Collections.singletonList(actuatorTypeDTO));
 
     // Act & Assert
     mockMvc.perform(get("/actuator-type/all")
             .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
+        .andExpect(jsonPath("$._embedded.actuatorTypeDTOList").exists())
         .andExpect(jsonPath("$._links.self").exists());
   }
 
   /**
-   * This test case verifies that the ActuatorTypeController returns a 404 Not Found status when no
-   * actuator types are available.
+   * This test case verifies that the ActuatorTypeController returns a not found status when no actuator types are available.
    */
   @Test
   void shouldReturnNotFound_WhenNoActuatorTypesAvailable() throws Exception {
-    // Arrange: Configure the service to return an empty list when called
-    when(actuatorTypeService.getAllActuatorTypes()).thenReturn(List.of());
+    when(actuatorTypeService.getAllActuatorTypes()).thenReturn(Collections.emptyList());
 
-    // Act & Assert: Perform the request and expect a 404 Not Found status
     mockMvc.perform(get("/actuator-type/all")
             .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound());
