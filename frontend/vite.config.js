e408653317b1a3ef14618d0cb44e07f5cb094a36
rueloParente <1231858@isep.ipp.pt>
2024-06-03 23:37:00 +0100

    #506 updated pom to build frontend resources

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+    // Make the build location be ../src/main/resources/static
+    build: {
+        outDir: '../src/main/resources/static'
+    }
 })
