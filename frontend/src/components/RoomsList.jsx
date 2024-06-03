11e6954a4bb35ef993a8f7a48fa416b9a065b6de
BernardoAmorim <1110281@isep.ipp.pt>
2024-06-03 11:53:40 +0100

    feat: Refactor RoomsList component to improve loading, error, and data handling conditions
    #510

--- frontend/src/components/RoomsList.jsx
+++ frontend/src/components/RoomsList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, {useContext, useEffect} from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,9 +6,9 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import AppContext from '../context/AppContext.jsx';
-import { fetchRooms, setCurrentRoom } from '../context/Actions.jsx';
+import {fetchRooms, setCurrentRoom} from '../context/Actions.jsx';
 
 function RoomsList() {
     const { state, dispatch } = useContext(AppContext);
@@ -29,54 +29,60 @@ function RoomsList() {
         navigate(`/rooms/${roomId}/devices`);
     };
 
-    if (loading === true) {
+    if (loading) {
         return <h1>Loading ....</h1>;
-    } else {
-        if (error !== null) {
-            return <h1>Error ....</h1>;
-        } else {
-            if (data.length > 0) {
-                return (
-                    <div>
-                        {data.map((room) => (
-                            <Accordion key={room.roomId} sx={{borderRadius: '5px', border: '2px solid black', backgroundColor:'white', fontFamily: 'Roboto, sans-serif', marginBottom: 2 }}>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls={`panel${room.roomId}-content`}
-                                    id={`panel${room.roomId}-header`}
-                                >
-                                    <Typography sx={{ fontFamily: 'Roboto, sans-serif' }}>
-                                        {room.roomName}
-                                    </Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <Typography sx={{ fontFamily: 'Roboto, sans-serif' }}>
-                                        Floor: {room.floor}
-                                    </Typography>
-                                </AccordionDetails>
-                                <AccordionActions>
-                                    <Button
-                                        size="small"
-                                        color="primary"
-                                        onClick={() => handleOpenDevicesClick(room.roomId, room.roomName)}>
-                                        Open Devices
-                                    </Button>
-                                    <Button
-                                        size="small"
-                                        color="primary"
-                                        onClick={() => handleAddDeviceClick(room.roomId)}>
-                                        Add Device
-                                    </Button>
-                                </AccordionActions>
-                            </Accordion>
-                        ))}
-                    </div>
-                );
-            } else {
-                return <h1>No data ....</h1>;
-            }
-        }
     }
+
+    if (error) {
+        return <h1>Error: {error}</h1>;
+    }
+
+    if (data.length === 0) {
+        return <h1>No data ....</h1>;
+    }
+
+    return (
+        <div>
+            {data.map((room) => (
+                <Accordion key={room.roomId} sx={{
+                    borderRadius: '5px',
+                    border: '2px solid black',
+                    backgroundColor: 'white',
+                    fontFamily: 'Roboto, sans-serif',
+                    marginBottom: 2
+                }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls={`panel${room.roomId}-content`}
+                        id={`panel${room.roomId}-header`}
+                    >
+                        <Typography sx={{fontFamily: 'Roboto, sans-serif'}}>
+                            {room.roomName}
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography sx={{fontFamily: 'Roboto, sans-serif'}}>
+                            Floor: {room.floor}
+                        </Typography>
+                    </AccordionDetails>
+                    <AccordionActions>
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={() => handleOpenDevicesClick(room.roomId, room.roomName)}>
+                            Open Devices
+                        </Button>
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={() => handleAddDeviceClick(room.roomId)}>
+                            Add Device
+                        </Button>
+                    </AccordionActions>
+                </Accordion>
+            ))}
+        </div>
+    );
 }
 
 export default RoomsList;
