beb68b0cdb7886f955b8d42bf527b69506ea3ab3
João Mouta <1231833@isep.ipp.pt>
2024-06-15 16:52:11 +0100

    Update cors Config

--- src/main/java/smarthome/utils/CorsConfig.java
+++ src/main/java/smarthome/utils/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
       }
     };
   }
